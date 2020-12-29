fcdf1a344fc482c1931f015b6699c69937432a38
Steve Jones <steve@stevejonesdev.com>
2020-12-29 11:10:48 -0500

    missing headings rule check only the_content, removed check for h1

--- includes/rules/missing_headings.php
+++ includes/rules/missing_headings.php
@@ -3,16 +3,18 @@
 function edac_rule_missing_headings($content, $post){
 
 	// rule vars
-	$dom = $content;
-	$h1 = count($dom->find('h1'));
+	$dom = str_get_html($post->post_content);
+	if(empty($dom)) goto error;
+	
 	$h2 = count($dom->find('h2'));
 	$h3 = count($dom->find('h3'));
 	$h4 = count($dom->find('h4'));
 	$h5 = count($dom->find('h5'));
 	$h6 = count($dom->find('h6'));	
-	$headings = ($h1+$h2+$h3+$h4+$h5+$h6);
+	$headings = ($h2+$h3+$h4+$h5+$h6);
 
 	if($headings == 0){
+		error:
 		$errorcode = __('Missing headings - Post ID: ', 'edac').$post->ID;
 		return [$errorcode];
 	}
