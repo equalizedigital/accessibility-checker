8c68375aeb8ea81c6d38acb59b5f81ea573179a5
Steve Jones <steve@stevejonesdev.com>
2021-01-05 16:30:39 -0500

    reworking missing title

--- includes/rules/missing_title.php
+++ includes/rules/missing_title.php
@@ -8,8 +8,25 @@
  * @return array
  */
 function edac_rule_missing_title($content, $post){
-    $title = get_the_title( $post->ID );
-    if(!$title || $title == ''){
-        return ["Missing Title - Post ID: $post->ID"];
-    }
+
+	$the_title = get_the_title( $post->ID );
+	$title = $content->find('title',0);
+	$meta_title = $content->find('meta[property="og:title"]',0);
+	//edac_log($title->innertext);
+	//edac_log($meta_title->getAttribute('content'));
+
+	if(!$the_title || $the_title == ''){
+		
+	}
+
+	if(
+		isset($title) && $title->innertext == ""
+		|| isset($meta_title) && $meta_title->hasAttribute('content') && $meta_title->getAttribute('content') == ""
+	)
+
+
+	
+	
+		//return ["Missing Title - Post ID: $post->ID"];
+	}
 }
