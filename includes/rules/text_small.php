521bc8a8f037643c4b12ee10ebec4252b1aa4b0f
Steve Jones <steve@stevejonesdev.com>
2021-01-07 17:36:22 -0500

    working on text small font size logic

--- includes/rules/text_small.php
+++ includes/rules/text_small.php
@@ -98,7 +98,12 @@ function ac_css_small_text_check($content, $styles){
 		foreach ($css_array as $element => $rules) {
 			if (array_key_exists('font-size', $rules)) {
 
-				if(preg_match('(rem|em|%|inherit)', $rules['font-size']) === 1) { continue; } 
+				//if(preg_match('(rem|em|%|inherit)', $rules['font-size']) === 1) { continue; } 
+				edac_log($rules['font-size']);
+				//edac_log($element);
+				if($element == 'body'){
+					edac_log($rules['font-size']);
+				}
 
 				if($rules['font-size'] <= 10){
 
@@ -124,3 +129,38 @@ function ac_css_small_text_check($content, $styles){
 	return $errors;
 
 }
+
+/*
+<absolute-size> values
+font-size: xx-small;
+font-size: x-small;
+font-size: small;
+font-size: medium;
+font-size: large;
+font-size: x-large;
+font-size: xx-large;
+font-size: xxx-large;
+
+<relative-size> values
+font-size: smaller;
+font-size: larger;
+
+<length> values
+font-size: 12px;
+font-size: 0.8em;
+
+<percentage> values
+font-size: 80%;
+
+Global values
+font-size: inherit;
+font-size: initial;
+font-size: unset;
+
+
+em and ex = font size of parent
+rem = html font size
+% = relative to parent font size
+
+font-size: 62.5%; font-size 1em = 10px on default browser settings
+*/
