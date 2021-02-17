7425677868e1390697fb644a7a64acf070137dee
Steve Jones <steve@stevejonesdev.com>
2021-02-17 11:43:38 -0500

    broken_aira_reference update function prefix

--- includes/rules/broken_aria_reference.php
+++ includes/rules/broken_aria_reference.php
@@ -8,14 +8,14 @@ function edac_rule_broken_aria_reference($content, $post){
     $labelledby_elements = $dom->find( '[aria-labelledby]' );
 
     foreach( $labelledby_elements as $labelledby_element ){
-      if( ! ac_has_all_referenced_elements( $labelledby_element, $dom, 'aria-labelledby' ) ) {
+      if( ! edac_has_all_referenced_elements( $labelledby_element, $dom, 'aria-labelledby' ) ) {
         $errors[] = $labelledby_element;
       }  
     } 
     
     $describedby_elements = $dom->find( '[aria-describedby]' );
     foreach( $describedby_elements as $describedby_element ){
-      if( ! ac_has_all_referenced_elements( $describedby_element, $dom, 'aria-describedby' ) ) {
+      if( ! edac_has_all_referenced_elements( $describedby_element, $dom, 'aria-describedby' ) ) {
         $errors[] = $describedby_element;
       }
        
@@ -23,7 +23,7 @@ function edac_rule_broken_aria_reference($content, $post){
     return $errors;
 }
 
-function ac_has_all_referenced_elements( $element, $dom, $attr = 'aria-labelledby' ) {
+function edac_has_all_referenced_elements( $element, $dom, $attr = 'aria-labelledby' ) {
     $label_string = $element->getAttribute($attr);
     $labels = explode( ' ', $label_string );
     foreach( $labels as $label ) {
