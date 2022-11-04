17076632e0c9767386165ea28953b7864d316c4c
Steve Jones <steve@stevejonesdev.com>
2022-11-04 11:10:45 -0400

    Add support for elements with a role of heading and aria-level defined #52

--- includes/rules/missing_headings.php
+++ includes/rules/missing_headings.php
@@ -6,11 +6,11 @@ function edac_rule_missing_headings($content, $post){
 	$dom = str_get_html($post->post_content);
 	if(empty($dom)) goto error;
 	
-	$h2 = count($dom->find('h2'));
-	$h3 = count($dom->find('h3'));
-	$h4 = count($dom->find('h4'));
-	$h5 = count($dom->find('h5'));
-	$h6 = count($dom->find('h6'));	
+	$h2 = count($dom->find('h2,[role=heading][aria-level=2]'));
+	$h3 = count($dom->find('h3,[role=heading][aria-level=3]'));
+	$h4 = count($dom->find('h4,[role=heading][aria-level=4]'));
+	$h5 = count($dom->find('h5,[role=heading][aria-level=5]'));
+	$h6 = count($dom->find('h6,[role=heading][aria-level=6]'));
 	$headings = ($h2+$h3+$h4+$h5+$h6);
 
 	if($headings == 0){
