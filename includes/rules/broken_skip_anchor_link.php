93123dba5556956d5f1167b9a9f6f446f7c89180
Steve Jones <steve@stevejonesdev.com>
2022-11-17 16:59:57 -0500

    exclude: '<a href="#"></a>' and '<a></a>' checked by the link_improper rule #61

--- includes/rules/broken_skip_anchor_link.php
+++ includes/rules/broken_skip_anchor_link.php
@@ -8,6 +8,11 @@ function edac_rule_broken_skip_anchor_link($content, $post){
 
     foreach( $anchor_elements as $anchor_element ){
         $href = $anchor_element->getAttribute('href');
+
+        // exclude: '<a href="#"></a>' and '<a></a>' checked by the link_improper rule
+        if(trim($anchor_element->getAttribute('href')) == "#" || (trim($anchor_element->getAttribute('href')) == "#" && $anchor_element->getAttribute('role') == "button")){
+            continue;
+        }
         
         if( substr( $href, 0, 1 ) == '#' ) {
             if( ! $dom->find( $href, 0 ) ) {
