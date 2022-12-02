c65fa5838eafd683e504139cc77f8a139687d47d
Steve Jones <steve@stevejonesdev.com>
2022-12-02 14:52:52 -0500

    updated to check for aria-expanded = true or false #79

--- includes/rules/link_improper.php
+++ includes/rules/link_improper.php
@@ -3,20 +3,23 @@
 /**
  * Improper Use of Link
  * 
- * Find all of the links on the page that contain only a # in the href attribute or are missing an href attribute completely. If these links do not have role="button" then flag an Improper Use of Link error. Though it would be better if it were an actual button element, adding role="menuitem" and aria-haspopup="true" to a link is sufficient for screen reader users also.
+ * Find all of the links on the page that contain only a # in the href attribute or are missing an href attribute completely. If these links do not have role="button" then flag an Improper Use of Link error. Though it would be better if it were an actual button element, adding aria-expanded="true" or aria-expanded="false" and aria-haspopup="true" to a link is sufficient for screen reader users also.
  * 
  * <a>improper</a>
  * <a role="button">improper</a>
  * <a href="#">improper</a>
  * <a href="#" role="menuitem">improper</a>
  * <a href="#" aria-haspopup="true">improper</a>
- * <a href="#" role="menuitem" aria-haspopup="true">proper</a>
+ * <a href="#" aria-expanded="true" aria-haspopup="true">proper</a>
+ * <a href="#" aria-expanded="false" aria-haspopup="true">proper</a>
  * <a href="#" role="button">proper</a>
  *
  * @param array $content
  * @param array $post
  * @return void
  */
+
+ // aria-expanded
 function edac_rule_link_improper($content, $post){
 	$dom = $content['html'];
 	$errors = [];
@@ -27,7 +30,7 @@ function edac_rule_link_improper($content, $post){
 			$error = true;
 		}elseif(trim($link->getAttribute('href')) == '#'){
 			$error = true;
-			if($link->getAttribute('role') == "button" || ($link->getAttribute('role') == 'menuitem' && $link->getAttribute('aria-haspopup') == "true")){
+			if($link->getAttribute('role') == "button" || (($link->getAttribute('aria-expanded') == "true" || $link->getAttribute('aria-expanded') == "false") && $link->getAttribute('aria-haspopup') == "true")){
 				$error = false;
 			}
 		}
