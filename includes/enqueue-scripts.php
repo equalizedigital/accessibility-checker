74143034593926d81d311c133d7e6aad43c58993
Steve Jones <steve@stevejonesdev.com>
2023-04-25 11:40:35 -0400

    updated - admin js enque to only load on needed pages

--- includes/enqueue-scripts.php
+++ includes/enqueue-scripts.php
@@ -17,16 +17,31 @@ function edac_admin_enqueue_styles() {
  */
 function edac_admin_enqueue_scripts() {
 
-	global $post;
-	$post_id = is_object( $post ) ? $post->ID : null;
-	wp_enqueue_script( 'edac', plugin_dir_url( __DIR__ ) . 'assets/js/accessibility-checker-admin-min.js', array( 'jquery' ), EDAC_VERSION, false );
-
-	wp_localize_script(
-		'edac',
-		'edac_script_vars',
-		array(
-			'postID' => $post_id,
-			'nonce'  => wp_create_nonce( 'ajax-nonce' ),
-		)
+	$post_types        = get_option( 'edac_post_types' );
+	$current_post_type = get_post_type();
+	$page              = isset( $_GET['page'] ) ? $_GET['page'] : null;
+	$pages             = array(
+		'accessibility_checker',
+		'accessibility_checker_settings',
+		'accessibility_checker_issues',
+		'accessibility_checker_ignored',
 	);
+
+	if ( in_array( $current_post_type, $post_types, true ) || in_array( $page, $pages, true ) ) {
+
+		global $post;
+		$post_id = is_object( $post ) ? $post->ID : null;
+		wp_enqueue_script( 'edac', plugin_dir_url( __DIR__ ) . 'assets/js/accessibility-checker-admin-min.js', array( 'jquery' ), EDAC_VERSION, false );
+
+		wp_localize_script(
+			'edac',
+			'edac_script_vars',
+			array(
+				'postID' => $post_id,
+				'nonce'  => wp_create_nonce( 'ajax-nonce' ),
+			)
+		);
+
+	}
+
 }
