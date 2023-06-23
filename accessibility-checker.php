5d263a7ce7b65c0a9b7714acd69abdcea5490c0c
matt <matt@moja.com>
2023-06-23 14:28:48 -0500

    Fixes #185

--- accessibility-checker.php
+++ accessibility-checker.php
@@ -821,16 +821,7 @@ function edac_summary_ajax() {
 		$html['password_protected'] = $notice_text;
 		$html['content']           .= '<div class="edac-summary-notice">' . $notice_text . '</div>';
 	}
-/*
-	$post_id = intval( $_REQUEST['post_id'] );
-	$summary = edac_summary( $post_id );
 
-	if ( $summary['readability'] <= 9 ) {
-		$simplified_summary_text = 'Your content has a reading level at or below 9th grade and does not require a simplified summary.';
-	} else {
-		$simplified_summary_text = $summary['simplified_summary'] ? 'A Simplified summary has been included for this content.' : 'A Simplified summary has not been included for this content.';
-	}
-*/
 	$post_id = intval( $_REQUEST['post_id'] );
 	$summary = edac_summary( $post_id );
 	$simplified_summary_text = '';
@@ -902,14 +893,14 @@ function edac_summary_ajax() {
 	<div class="edac-summary-readability">
 		<div class="edac-summary-readability-level">
 			<div><img src="' . plugin_dir_url( __FILE__ ) . 'assets/images/readability icon navy.png" alt="" width="54"></div>
-			<div class="edac-panel-number' . ( ( $summary['readability'] <= 9 ) ? ' passed-text-color' : ' failed-text-color' ) . '">
+			<div class="edac-panel-number' . ( ( (int) $summary['readability'] <= 9 || 'none' === $simplified_summary_prompt ) ? ' passed-text-color' : ' failed-text-color' ) . '">
 				' . $summary['readability'] . '
 			</div>
-			<div class="edac-panel-number-label' . ( ( $summary['readability'] <= 9 ) ? ' passed-text-color' : ' failed-text-color' ) . '">Reading <br />Level</div>
+			<div class="edac-panel-number-label' . ( ( (int) $summary['readability'] <= 9 || 'none' === $simplified_summary_prompt ) ? ' passed-text-color' : ' failed-text-color' ) . '">Reading <br />Level</div>
 		</div>
 		<div class="edac-summary-readability-summary">
-			<div class="edac-summary-readability-summary-icon' . ( ('none' !== $simplified_summary_prompt && ( $summary['simplified_summary'] || $summary['readability'] <= 9 ) ) ? ' active' : '' ) . '"></div>
-			<div class="edac-summary-readability-summary-text' . ( ('none' !== $simplified_summary_prompt && ( $summary['simplified_summary'] || $summary['readability'] <= 9 ) ) ? ' active' : '' ) . '">' . $simplified_summary_text . '</div>
+			<div class="edac-summary-readability-summary-icon' . ( ( 'none' === $simplified_summary_prompt || $summary['simplified_summary'] || (int) $summary['readability'] <= 9 ) ? ' active' : '' ) . '"></div>
+			<div class="edac-summary-readability-summary-text' . ( ( 'none' === $simplified_summary_prompt || $summary['simplified_summary'] || (int) $summary['readability'] <= 9 ) ? ' active' : '' ) . '">' . $simplified_summary_text . '</div>
 		</div>
 	</div>
 	<div class="edac-summary-disclaimer"><small>* True accessibility requires manual testing in addition to automated scans. <a href="https://a11ychecker.com/help4280">Learn how to manually test for accessibility</a>.</small></div>
