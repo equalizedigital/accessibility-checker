167fefe0a3f72a8bd2f8359c8bb60ffbe7fe22bc
matt <matt@moja.com>
2023-10-23 10:05:08 -0500

    Add files

---
+++ install.sh
@@ -0,0 +1,17 @@
+mkdir -p ./dist
+mkdir -p ./tests/e2e/.auth
+mkdir -p ./patches
+mkdir -p ./.wp-env
+
+# note, when pulling from github you must include the commit hash
+# https://stackoverflow.com/questions/22608398/composer-update-not-pulling-latest-dev-master
+composer upgrade
+
+cp -r -n ./vendor/equalizedigital/accessibility-checker-wp-env/.wp-env/ ./.wp-env 
+cp -r -n ./vendor/equalizedigital/accessibility-checker-wp-env/.wp-env.json ./
+cp -r ./vendor/equalizedigital/accessibility-checker-wp-env/patches/* ./patches/
+
+npm run start
+./.wp-env/scripts/init.sh
+npm run build
+
