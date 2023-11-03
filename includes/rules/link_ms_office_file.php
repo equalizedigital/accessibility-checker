7c829cc02fe1ab0f0d5c4bf8855270c3765f6a5e
Steve Jones <steve@stevejonesdev.com>
2023-11-03 12:22:43 -0400

    updated - logic for the link to MS Office file #336

--- includes/rules/link_ms_office_file.php
+++ includes/rules/link_ms_office_file.php
@@ -14,21 +14,28 @@
  */
 function edac_rule_link_ms_office_file( $content, $post ) {
 
-	$dom = $content['html'];
+	$dom             = $content['html'];
 	$file_extensions = array( '.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx', '.pps', '.ppsx' );
-	$errors = array();
+	$errors          = array();
 
 	$as = $dom->find( 'a' );
 	foreach ( $as as $a ) {
+		// Get the href attribute of the anchor tag.
+		$href = $a->getAttribute( 'href' );
+		if ( $href ) {
+			// Remove any query or fragment from the URL.
+			$url_components = wp_parse_url( $href );
+			$clean_path     = $url_components['path'] ?? '';
 
-		if ( $a->getAttribute( 'href' ) ) {
-			if ( $file_extensions ) {
-				foreach ( $file_extensions as $file_extension ) {
-					if ( strpos( strtolower( $a ), $file_extension ) ) {
-						$link_code = $a;
-
-						$errors[] = $link_code;
-					}
+			// Check if cleaned URL contains any file extension.
+			foreach ( $file_extensions as $file_extension ) {
+				// Check if the cleaned path ends with a file extension.
+				if ( substr( strtolower( $clean_path ), -strlen( $file_extension ) ) === $file_extension ) {
+					// If match found, store the outer HTML of the anchor tag.
+					$link_code = $a->outertext;
+					$errors[]  = $link_code;
+					// No need to check other extensions for this link.
+					break;
 				}
 			}
 		}
