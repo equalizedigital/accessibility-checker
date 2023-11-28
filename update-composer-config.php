855011c8621392eb36f1111a4a9e9c2e67631cc6
Steve Jones <steve@stevejonesdev.com>
2023-11-28 16:54:08 -0500

    updated - phpcs to exclude update-composer-config.php

--- update-composer-config.php
+++ update-composer-config.php
@@ -9,12 +9,12 @@
 $env = getenv( 'GITHUB_ACTIONS' );
 
 $composer_json_path = 'composer.json';
-$composer_config = json_decode( file_get_contents( $composer_json_path ), true );
+$composer_config    = json_decode( file_get_contents( $composer_json_path ), true );
 
 if ( 'true' === $env ) {
 	// Running in GitHub Actions environment.
 	// Remove local specific packages for GitHub Actions environment.
-	unset($composer_config['require-dev']['equalizedigital/accessibility-checker-wp-env']);
+	unset( $composer_config['require-dev']['equalizedigital/accessibility-checker-wp-env'] );
 } else {
 	// Not running in GitHub Actions, assuming local environment.
 	// Add your local specific packages.
@@ -22,4 +22,3 @@ if ( 'true' === $env ) {
 }
 
 file_put_contents( $composer_json_path, json_encode( $composer_config, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES ) );
-
