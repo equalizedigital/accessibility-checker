4ed1d51c97bfd73d155ce5e20768fe4aa9e76682
Ari Stathopoulos <aristath@gmail.com>
2023-12-08 12:18:25 +0200

    Basic tests for the edac_compare_strings function

---
+++ tests/phpunit/helper-function/test-edac_compare_strings.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Accessibility_Checker
+ */
+
+/**
+ * Sample test case.
+ */
+class EDACCompareStrings extends WP_UnitTestCase {
+
+	/**
+	 * Tests the edac_compare_strings function.
+	 * 
+	 * @dataProvider edac_compare_strings_data
+	 * 
+	 * @param string $string1  The 1st string to compare.
+	 * @param string $string2  The 2nd string to compare.
+	 * @param string $expected The expected result of the comparison.
+	 */
+	public function test_edac_compare_strings( $string1, $string2, $expected ) {
+		$this->assertSame(
+			$expected, 
+			edac_compare_strings( $string1, $string2 ) 
+		);
+	}
+
+	/**
+	 * Data provider for test_edac_compare_strings.
+	 */
+	public function edac_compare_strings_data() {
+		return array(
+			'mixed upper/lower letters' => array(
+				'string1'  => 'random string',
+				'string2'  => 'RanDom StrIng',
+				'expected' => true,
+			),
+			'different casing with same html tags' => array(
+				'string1'  => '<p>random string</p>',
+				'string2'  => '<p>RanDom StrIng</p>',
+				'expected' => true,
+			),
+			'with different html tags' => array(
+				'string1'  => '<p>random string</p>',
+				'string2'  => '<div>random string</div>',
+				'expected' => true,
+			),
+			'containing "permalink of/to" strings' => array(
+				'string1'  => 'permalink of random string',
+				'string2'  => 'permalink to random string',
+				'expected' => true,
+			),
+			'containing "&nbsp;" strings' => array(
+				'string1'  => 'random&nbsp; string',
+				'string2'  => 'random string',
+				'expected' => true,
+			),
+			'different strings' => array(
+				'string1'  => 'random string',
+				'string2'  => 'different string',
+				'expected' => false,
+			),
+		);
+	}
+}
