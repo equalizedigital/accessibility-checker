4948d50cb433668714e27c7d59bf4b4b6d216461
Steve Jones <steve@stevejonesdev.com>
2023-12-12 13:19:09 -0500

    updated - class to use init_hooks mehtod and added simple unit test

---
+++ tests/phpunit/test-admin_notices.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Accessibility_Checker
+ */
+
+/**
+ * Sample test case.
+ */
+class EDACAdminNoticesTest extends WP_UnitTestCase {
+    
+    private $admin_notices;
+
+    protected function setUp(): void {
+        $this->admin_notices = new Admin_Notices();
+    }
+
+    public function test_edac_get_black_friday_message_exists() {
+        $this->assertTrue(
+            method_exists( $this->admin_notices, 'edac_get_black_friday_message'),
+            'Class does not have method edac_get_black_friday_message'
+        );
+    }
+
+    public function test_edac_get_black_friday_message_returns_string() {
+        $this->assertIsString( $this->admin_notices->edac_get_black_friday_message() );
+    }
+
+    public function test_edac_get_black_friday_message_contains_promo_message() {
+        $message = $this->admin_notices->edac_get_black_friday_message();
+        $this->assertStringContainsString( 'Black Friday special!', $message );
+        $this->assertStringContainsString( 'Upgrade to a paid version of Accessibility Checker', $message );
+    }
+}
