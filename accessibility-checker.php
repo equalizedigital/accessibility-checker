0ae0c41430765160c846122d1fdc1c86e3545562
Steve Jones <steve@stevejonesdev.com>
2023-12-16 01:20:28 -0500

    removed - classes require_once

--- accessibility-checker.php
+++ accessibility-checker.php
@@ -128,17 +128,6 @@ require_once plugin_dir_path( __FILE__ ) . 'includes/validate.php';
 require_once plugin_dir_path( __FILE__ ) . 'includes/insert.php';
 require_once plugin_dir_path( __FILE__ ) . 'includes/purge.php';
 require_once plugin_dir_path( __FILE__ ) . 'includes/system-info.php';
-//require_once plugin_dir_path( __FILE__ ) . 'includes/classes/class-admin-notices.php';
-//require_once plugin_dir_path( __FILE__ ) . 'includes/classes/class-rest-api.php';
-//require_once plugin_dir_path( __FILE__ ) . 'includes/classes/class-helpers.php';
-//require_once plugin_dir_path( __FILE__ ) . 'includes/classes/class-settings.php';
-//require_once plugin_dir_path( __FILE__ ) . 'includes/classes/class-issues-query.php';
-//require_once plugin_dir_path( __FILE__ ) . 'includes/classes/class-scans-stats.php';
-//require_once plugin_dir_path( __FILE__ ) . 'includes/classes/class-widgets.php';
-//require_once plugin_dir_path( __FILE__ ) . 'includes/classes/class-welcome-page.php';
-//require_once plugin_dir_path( __FILE__ ) . 'includes/classes/class-ajax.php';
-//require_once plugin_dir_path( __FILE__ ) . 'includes/classes/class-frontend-highlight.php';
-//require_once plugin_dir_path( __FILE__ ) . 'includes/classes/class-accessibility-statement.php';
 
 /**
  * Filters and Actions
