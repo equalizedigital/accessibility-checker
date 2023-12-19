bbc43d8ddb3882a92112317b62ff98dbf0564c2b
matt <matt@moja.com>
2023-12-19 13:52:46 -0600

    Bugfix  - PHP Deprecated:  Automatic conversion of false to array

--- includes/classes/class-scans-stats.php
+++ includes/classes/class-scans-stats.php
@@ -371,7 +371,7 @@ class Scans_Stats {
 	
 		$transient_name = $this->cache_name_prefix . '_issues_summary_by_post_type_' . $post_type;
 
-		$cache = get_transient( $transient_name );
+		$cache = get_transient( $transient_name, false );
 	
 		if ( $this->cache_time && false !== $cache ) {
 
