7b07115a9a4d7986ec9e474e79c4908f467ebf43
matt <matt@moja.com>
2023-12-22 11:03:11 -0600

    Update file/folder names

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -6,18 +6,18 @@ mkdir ./dist
 npx wp-scripts plugin-zip
 
 # unzip the zip into its own folder so we can zip that
-unzip accessibility-checker-audit.zip -d ./dist/accessibility-checker-audit 
+unzip accessibility-checker.zip -d ./dist/accessibility-checker
 
 # plugin-zip includes package.json which is not needed for the plugin, so remove.
-rm ./dist/accessibility-checker-pro/package.json
+rm ./dist/accessibility-checker/package.json
 
 #remove the original zip
 rm accessibility-checker.zip
 
 #move into the dist folder and zip the plugin's folder
 cd ./dist
-zip -r accessibility-checker-audit.zip ./accessibility-checker-audit
+zip -r accessibility-checker.zip ./accessibility-checker
 
 #cleanup and drop back into the original dir
-rm -r ./accessibility-checker-audit
+rm -r ./accessibility-checker
 cd ..
