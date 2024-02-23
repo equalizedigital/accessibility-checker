741400cd8767864901b9ccba26a2cd41dd46d5ea
pattonwebz <will@pattonwebz.com>
2024-02-23 23:18:45 +0000

    Create a class to hold the insert rule data function

---
+++ admin/class-insert-rule-data.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Inserts rule data about a post to the database
+ *
+ * @package Accessibility_Checker
+ */
+
+namespace EDAC\Admin;
+
+/**
+ * Class for inserting rule data into the database
+ */
+class Insert_Rule_Data {
+
+}
