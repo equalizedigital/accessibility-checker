38e6fe7b5ab11fa22b3437ac1d824519c262671d
pattonwebz <will@pattonwebz.com>
2024-02-23 22:51:43 +0000

    Replace the code in the new deprecated methods with calls to the replacements

--- includes/deprecated/deprecated.php
+++ includes/deprecated/deprecated.php
@@ -6,6 +6,8 @@
  * @package Accessibility_Checker
  */
 
+use EDAC\Admin\Purge_Post_Data;
+
 /**
  * Alias of the is_plugin_active() function.
  *
@@ -42,16 +44,8 @@ function edac_summary( $post_id ) {
  * @return void
  */
 function edac_delete_post( $post_id ) {
-	_deprecated_function( __FUNCTION__, '1.9.0', 'Purge_Post_Data::delete_post' );
-
-	global $wpdb;
-	$site_id    = get_current_blog_id();
-	$table_name = $wpdb->prefix . 'accessibility_checker';
-
-	// phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery, WordPress.DB.DirectDatabaseQuery.NoCaching -- Safe variable used for table name, caching not required for one time operation.
-	$wpdb->query( $wpdb->prepare( 'DELETE FROM %i WHERE postid = %d and siteid = %d', $table_name, $post_id, $site_id ) );
-
-	edac_delete_post_meta( $post_id );
+	_deprecated_function( __FUNCTION__, '1.9.0', 'EDAC\Admin\Purge_Post_Data::delete_post' );
+	Purge_Post_Data::delete_post( $post_id );
 }
 
 /**
@@ -63,20 +57,8 @@ function edac_delete_post( $post_id ) {
  * @return void
  */
 function edac_delete_post_meta( $post_id ) {
-	_deprecated_function( __FUNCTION__, '1.9.0', 'Purge_Post_Data::delete_post_meta' );
-
-	if ( ! $post_id ) {
-		return;
-	}
-
-	$post_meta = get_post_meta( $post_id );
-	if ( $post_meta ) {
-		foreach ( $post_meta as $key => $value ) {
-			if ( substr( $key, 0, 5 ) === '_edac' || substr( $key, 0, 6 ) === '_edacp' ) {
-				delete_post_meta( $post_id, $key );
-			}
-		}
-	}
+	_deprecated_function( __FUNCTION__, '1.9.0', 'EDAC\Admin\Purge_Post_Data::delete_post_meta' );
+	Purge_Post_Data::delete_post_meta( $post_id );
 }
 
 /**
@@ -88,23 +70,6 @@ function edac_delete_post_meta( $post_id ) {
  * @return void
  */
 function edac_delete_cpt_posts( $post_type ) {
-	_deprecated_function( __FUNCTION__, '1.9.0', 'Purge_Post_Data::delete_cpt_posts' );
-
-	if ( ! $post_type ) {
-		return;
-	}
-
-	global $wpdb;
-	$site_id = get_current_blog_id();
-
-	// phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery, WordPress.DB.DirectDatabaseQuery.NoCaching -- Safe variable used for table name, caching not required for one time operation.
-	return $wpdb->query(
-		$wpdb->prepare(
-			"DELETE T1,T2 from $wpdb->postmeta as T1 JOIN %i as T2 ON t1.post_id = T2.postid WHERE t1.meta_key like %s and T2.siteid=%d and T2.type=%s",
-			edac_get_valid_table_name( $wpdb->prefix . 'accessibility_checker' ),
-			'_edac%',
-			$site_id,
-			$post_type
-		)
-	);
+	_deprecated_function( __FUNCTION__, '1.9.0', 'EDAC\Admin\Purge_Post_Data::delete_cpt_posts' );
+	Purge_Post_Data::delete_cpt_posts( $post_type );
 }
