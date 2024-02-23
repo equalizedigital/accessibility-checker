29f5968554cd8dfc667e954e92a66689e2ff0a41
pattonwebz <will@pattonwebz.com>
2024-02-23 16:39:43 +0000

    Add a new class to hold the purge methods that are being deprecated and shifted

---
+++ admin/class-purge-post-data.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Purge Post Data stored in the database that holds scan data about the posts.
+ *
+ * @package Accessibility_Checker
+ */
+
+namespace EDAC\Admin;
+
+/**
+ * Uses sql queries to get and purge post data from the database for given post ids or for custom posts by id.
+ */
+class Purge_Post_Data {
+
+}
