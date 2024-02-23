6a786c9d2c6342d55086fdb6597bcae048ecb73b
pattonwebz <will@pattonwebz.com>
2024-02-23 23:25:09 +0000

    Move the `edac_insert_rule_data` function to deprecated.php

--- includes/deprecated/deprecated.php
+++ includes/deprecated/deprecated.php
@@ -2,13 +2,15 @@
 /**
  * Functions that have been deprecated and should not be used.
  * They are still kept here for backwards-compatibility.
- * 
+ *
  * @package Accessibility_Checker
  */
 
+use EDAC\Admin\Insert_Rule_Data;
+
 /**
  * Alias of the is_plugin_active() function.
- * 
+ *
  * @deprecated 1.6.11
  *
  * @param string $plugin_slug The plugin slug.
@@ -21,7 +23,7 @@ function edac_check_plugin_active( $plugin_slug ) {
 
 /**
  * Summary Data
- * 
+ *
  * @deprecated 1.9.0
  *
  * @param int $post_id ID of the post.
@@ -31,4 +33,20 @@ function edac_summary( $post_id ) {
 	_deprecated_function( __FUNCTION__, '1.9.0', 'EDAC\Inc\Summary_Generator' );
 
 	return ( new EDAC\Inc\Summary_Generator( $post_id ) )->generate_summary();
+}
+
+/**
+ * Insert rule date into database
+ *
+ * @deprecated 1.9.0
+ *
+ * @param object $post     The post object.
+ * @param string $rule     The rule.
+ * @param string $ruletype The rule type.
+ * @param string $rule_obj The object.
+ * @return void|int
+ */
+function edac_insert_rule_data( $post, $rule, $ruletype, $rule_obj ) {
+	_deprecated_function( __FUNCTION__, '1.9.0', 'EDAC\Admin\Insert_Rule_Data' );
+	return ( new Insert_Rule_Data() )->insert( $post, $rule, $ruletype, $rule_obj );
 }
