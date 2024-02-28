227dcaa72ebccadac09a73483007401a62ef91ec
pattonwebz <will@pattonwebz.com>
2024-02-28 19:59:16 +0000

    Update test to verify when data goes into the database and that it is only inserted when expected

--- tests/phpunit/InsertRuleDataTest.php
+++ tests/phpunit/InsertRuleDataTest.php
@@ -13,6 +13,49 @@ use EDAC\Admin\Insert_Rule_Data;
 class InsertRuleDataTest extends WP_UnitTestCase {
 
 	/**
+	 * Create table to test against.
+	 *
+	 * @return void
+	 */
+	public function setUp(): void {
+		global $wpdb;
+		$this->table_name = $wpdb->prefix . 'accessibility_checker';
+		$charset_collate  = $wpdb->get_charset_collate();
+		$sql              = "CREATE TABLE $this->table_name (
+				id bigint(20) NOT NULL AUTO_INCREMENT,
+				postid bigint(20) NOT NULL,
+				siteid text NOT NULL,
+				type text NOT NULL,
+				rule text NOT NULL,
+				ruletype text NOT NULL,
+				object mediumtext NOT NULL,
+				recordcheck mediumint(9) NOT NULL,
+				created timestamp NOT NULL default CURRENT_TIMESTAMP,
+				user bigint(20) NOT NULL,
+				ignre mediumint(9) NOT NULL,
+				ignre_global mediumint(9) NOT NULL,
+				ignre_user bigint(20) NULL,
+				ignre_date timestamp NULL,
+				ignre_comment mediumtext NULL,
+				UNIQUE KEY id (id),
+				KEY postid_index (postid)
+			) $charset_collate;";
+
+		require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+		dbDelta( $sql );
+	}
+
+	/**
+	 * Cleans up the table after each test.
+	 *
+	 * @return void
+	 */
+	public function tearDown(): void {
+		global $wpdb;
+		$wpdb->query( "DROP TABLE IF EXISTS $this->table_name" ); // phpcs:ignore WordPress.DB -- Table name is safe and not caching in a test.
+	}
+
+	/**
 	 * Tests the insert method would return expected data types.
 	 */
 	public function testRuleInserterReturnLogic() {
@@ -21,17 +64,33 @@ class InsertRuleDataTest extends WP_UnitTestCase {
 		$ruletype = 'ruletype';
 		$rule_obj = 'rule_obj';
 
-		$rule_inserter = new Insert_Rule_Data();
+		sleep( 2 );
+		global $wpdb;
 
-		// first call should insert and return null.
+		$rule_inserter     = new Insert_Rule_Data();
+		$initial_row_count = $wpdb->get_var( "SELECT COUNT(*) FROM $this->table_name" ); // phpcs:ignore WordPress.DB -- caching not required for one time operation.
+
+		// call should return int as a successful insert.
 		$new_data = $rule_inserter->insert( $post, $rule, $ruletype, $rule_obj );
-		$this->assertEquals( null, $new_data );
-		// second call is a duplicate and should return the row id.
+		$this->assertIsInt( $new_data );
+		// second call is a duplicate and should return null.
 		$duplicate_data = $rule_inserter->insert( $post, $rule, $ruletype, $rule_obj );
-		$this->assertisInt( $duplicate_data );
+		$this->assertEquals( null, $duplicate_data );
 
-		// third call should throw an exception because of missing parameters.
+		// check if the row count has increased by 1.
+		$current_row_count = $wpdb->get_var( "SELECT COUNT(*) FROM $this->table_name" ); // phpcs:ignore WordPress.DB -- caching not required for one time operation.
+		$this->assertEquals( $initial_row_count + 1, $current_row_count );
+
+		// should return null as ruletype is 'revision'.
+		$revision_type_return = $rule_inserter->insert( $post, $rule, 'revision', $rule_obj );
+		$this->assertEquals( null, $revision_type_return );
+
+		// should throw an exception because of missing parameters.
 		$this->expectException( TypeError::class );
 		$rule_inserter->insert();
+
+		// check that row count has not increased since last check.
+		$current_row_count = $wpdb->get_var( "SELECT COUNT(*) FROM $this->table_name" ); // phpcs:ignore WordPress.DB -- caching not required for one time operation.
+		$this->assertEquals( $initial_row_count + 1, $current_row_count );
 	}
 }
