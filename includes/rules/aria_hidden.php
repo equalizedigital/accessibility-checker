6aa59e7af009da01e4fa8256478b32bc4f938d6d
pattonwebz <will@pattonwebz.com>
2024-03-07 20:50:38 +0000

    Make aria-hidden not flag elements if their parent has a non-empty label

--- includes/rules/aria_hidden.php
+++ includes/rules/aria_hidden.php
@@ -32,9 +32,36 @@ function edac_rule_aria_hidden( $content, $post ) { // phpcs:ignore -- $post is 
 				}
 			}
 
+			$parent_node = $element->parent();
+			if ( $parent_node && edac_rule_aria_hidden_valid_parentnode_condition_check( $parent_node, $element ) ) {
+				continue;
+			}
+
 			$errors[] = $element;
 		}
 	}
 
 	return $errors;
+}
+
+/**
+ * Check if the parent has a valid situation to avoid flagging aria-hidden="true" warning.
+ *
+ * @param object $parent_node  The parent element.
+ * @param object $element The element.
+ *
+ * @return bool
+ */
+function edac_rule_aria_hidden_valid_parentnode_condition_check( object $parent_node, object $element ): bool {
+	// bail early if we don't have a parent node or an element node.
+	if ( ! $parent_node || ! $element ) {
+		return false;
+	}
+
+	// parent node with a non-empty aria-label makes the aria-hidden="true" likely valid.
+	if ( ! empty( $parent_node->getAttribute( 'aria-label' ) ) ) {
+		return true;
+	}
+
+	return false;
 }
