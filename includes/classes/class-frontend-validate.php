52cf5beaa6d1c67d34e1f49dee70aa0ab9b83d8a
pattonwebz <will@pattonwebz.com>
2024-03-20 13:54:18 +0000

    Don't try to perform the initial frontend validate scan when post is an auto-draft

--- includes/classes/class-frontend-validate.php
+++ includes/classes/class-frontend-validate.php
@@ -33,9 +33,11 @@ class Frontend_Validate {
 	 * Validates the current post on the WordPress dashboard home under specific conditions.
 	 *
 	 * This function is triggered only when viewing the dashboard home ('index.php'), not in a customizer preview,
-	 * and if the current user has permissions to edit posts. It checks if the current post has been previously 
-	 * validated based on a specific post meta key ('_edac_post_checked'). If the post has not been validated, 
+	 * and if the current user has permissions to edit posts. It checks if the current post has been previously
+	 * validated based on a specific post meta key ('_edac_post_checked'). If the post has not been validated,
 	 * it initiates the validation process.
+	 *
+	 * @modified 1.10.0 Return early if post is an auto-draft.
 	 *
 	 * @return void The function does not return a value. It triggers validation for an unvalidated post or does nothing.
 	 * @since 1.9.0
@@ -47,8 +49,8 @@ class Frontend_Validate {
 		if ( 'index.php' === $pagenow && false === is_customize_preview() && current_user_can( 'edit_posts' ) ) {
 
 			global $post;
-			$post_id = is_object( $post ) ? $post->ID : null;       
-			if ( null === $post_id ) {
+			$post_id = is_object( $post ) ? $post->ID : null;
+			if ( null === $post_id || 'auto-draft' === $post->post_status ) {
 				return;
 			}
 
