e8a58b99d4f047d28169b1552b804e826ec50474
pattonwebz <will@pattonwebz.com>
2024-03-25 14:37:29 +0000

    Add tests for the new helper functions

---
+++ tests/phpunit/helper-functions/GetFileOpenedAsBinaryTest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Tests the opening of image files as binary.
+ *
+ * @package Accessibility_Checker
+ */
+
+/**
+ * Tests the function that opens a file as binary - used when analyzing images.
+ *
+ * @group helper-functions
+ */
+class GetFileOpenedAsBinaryTest extends WP_UnitTestCase {
+
+	/**
+	 * Test if file gets retrieved and opened as binary.
+	 *
+	 * @group external-http
+	 */
+	public function test_file_opened_as_binary() {
+		$file = 'https://httpbin.org/image/webp';
+
+		$fh = edac_get_file_opened_as_binary( $file );
+
+		$this->assertNotFalse( $fh );
+		fclose( $fh );
+	}
+
+	/**
+	 * Test if file is not opened as binary.
+	 *
+	 * @group external-http
+	 */
+	public function test_file_not_opened_as_binary() {
+		$file = 'https://httpbin.org/status/404';
+
+		$fh = edac_get_file_opened_as_binary( $file );
+
+		$this->assertFalse( $fh );
+	}
+}
