e8c5ccd912cc59ae72ff55546c7c0dba5051c159
pattonwebz <will@pattonwebz.com>
2024-03-25 18:31:34 +0000

    Add some tests that test the full edac_rule_img_animated_gif rule

---
+++ tests/phpunit/includes/rules/ImgAnimatedGifTest.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * Tests for the gif and webp animation rule.
+ *
+ * @package Accessibility_Checker
+ */
+
+/**
+ * Test that the animated gif rule detects animated gif and webp and does not count
+ * static gif and webp as an error.
+ */
+class ImgAnimatedGifTest extends WP_UnitTestCase {
+
+	/**
+	 * Test if animated gif is detected.
+	 */
+	public function testAnimatedGifDetectionWithAnimatedGif() {
+		$filename = EDAC_TEST_ASSETS_DIR . 'animated.gif';
+		$this->assertTrue( edac_img_gif_is_animated( $filename ) );
+	}
+
+	/**
+	 * Test that static gif is not detected as animated.
+	 */
+	public function testAnimatedGifDetectionWithStaticGif() {
+		$filename = EDAC_TEST_ASSETS_DIR . 'static.gif';
+		$this->assertFalse( edac_img_gif_is_animated( $filename ) );
+	}
+
+	/**
+	 * Test that animated webp is detected as animated.
+	 */
+	public function testAnimatedWebpDetectionWithAnimatedWebp() {
+		$filename = EDAC_TEST_ASSETS_DIR . 'animated.webp';
+		$this->assertTrue( edac_img_webp_is_animated( $filename ) );
+	}
+
+	/**
+	 * Test that static webp is not detected as animated.
+	 */
+	public function testAnimatedWebpDetectionWithStaticWebp() {
+		$filename = EDAC_TEST_ASSETS_DIR . 'static.webp';
+		$this->assertFalse( edac_img_webp_is_animated( $filename ) );
+	}
+
+	/**
+	 * Test that animated gif is detected when passed through entire rule.
+	 */
+	public function testRuleWithAnimatedGifInContent() {
+		$html    = '<img src="' . EDAC_TEST_ASSETS_DIR . 'animated.gif">';
+		$dom     = $this->get_DOM( $html );
+		$content = array( 'html' => $dom );
+		$post    = new stdClass();
+		$errors  = edac_rule_img_animated_gif( $content, $post );
+		$this->assertNotEmpty( $errors );
+	}
+
+	/**
+	 * Test that static gif is not detected as animated when passed through entire rule.
+	 */
+	public function testRuleWithStaticGifInContent() {
+		$html    = '<img src="' . EDAC_TEST_ASSETS_DIR . 'static.gif">';
+		$dom     = $this->get_DOM( $html );
+		$content = array( 'html' => $dom );
+		$post    = new stdClass();
+		$errors  = edac_rule_img_animated_gif( $content, $post );
+		$this->assertEmpty( $errors );
+	}
+
+	/**
+	 * Test that animated webp is detected as animated when passed through entire rule.
+	 */
+	public function testRuleWithAnimatedWebpInContent() {
+		$html    = '<img src="' . EDAC_TEST_ASSETS_DIR . 'animated.webp">';
+		$dom     = $this->get_DOM( $html );
+		$content = array( 'html' => $dom );
+		$post    = new stdClass();
+		$errors  = edac_rule_img_animated_gif( $content, $post );
+		$this->assertNotEmpty( $errors );
+	}
+
+	/**
+	 * Test that static webp is not detected as animated when passed through entire rule.
+	 */
+	public function testRuleWithStaticWebpInContent() {
+
+		$html    = '<img src="' . EDAC_TEST_ASSETS_DIR . 'static.webp">';
+		$dom     = $this->get_DOM( $html );
+		$content = array( 'html' => $dom );
+		$post    = new stdClass();
+		$errors  = edac_rule_img_animated_gif( $content, $post );
+		$this->assertEmpty( $errors );
+	}
+
+	/**
+	 * Wrapper to generate dom objects that match the shape of the object in the plugin.
+	 *
+	 * @param string $html_string HTML string.
+	 * @return EDAC_Dom
+	 */
+	private function get_DOM( string $html_string = '' ) {
+		return new EDAC_Dom(
+			$html_string,
+			true,
+			true,
+			DEFAULT_TARGET_CHARSET,
+			true,
+			DEFAULT_BR_TEXT,
+			DEFAULT_SPAN_TEXT
+		);
+	}
+}
