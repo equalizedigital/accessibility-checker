6d3f898430803986bced7de40669c4164793254d
Steve Jones <steve@stevejonesdev.com>
2024-03-26 23:25:36 -0400

    added - edac_max_alt_length filter and edac_rule_img_alt_long tests #95

--- includes/rules/img_alt_long.php
+++ includes/rules/img_alt_long.php
@@ -14,14 +14,16 @@
  */
 function edac_rule_img_alt_long( $content, $post ) { // phpcs:ignore -- $post is reserved for future use or for compliance with a specific interface.
 
-	$dom    = $content['html'];
-	$errors = array();
-	$images = $dom->find( 'img' );
+	$dom            = $content['html'];
+	$errors         = array();
+	$images         = $dom->find( 'img' );
+	$max_alt_length = absint( apply_filters( 'edac_max_alt_length', 300 ) );
+	$max_alt_length = max( 1, $max_alt_length );
 
 	foreach ( $images as $image ) {
 		if ( isset( $image ) && $image->hasAttribute( 'alt' ) && $image->getAttribute( 'alt' ) !== '' ) {
 			$alt = $image->getAttribute( 'alt' );
-			if ( strlen( $alt ) > 300 ) {
+			if ( strlen( $alt ) > $max_alt_length ) {
 				$image_code = $image;
 				$errors[]   = $image_code;
 			}
