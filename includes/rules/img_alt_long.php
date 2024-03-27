47efd33167d97881d79d6a01a18d0198650de153
Steve Jones <steve@stevejonesdev.com>
2024-03-27 10:17:07 -0400

    Update includes/rules/img_alt_long.php
    Co-authored-by: William Patton <will@pattonwebz.com>

--- includes/rules/img_alt_long.php
+++ includes/rules/img_alt_long.php
@@ -14,11 +14,18 @@
  */
 function edac_rule_img_alt_long( $content, $post ) { // phpcs:ignore -- $post is reserved for future use or for compliance with a specific interface.
 
-	$dom            = $content['html'];
-	$errors         = array();
-	$images         = $dom->find( 'img' );
-	$max_alt_length = absint( apply_filters( 'edac_max_alt_length', 300 ) );
-	$max_alt_length = max( 1, $max_alt_length );
+	$dom    = $content['html'];
+	$errors = array();
+	$images = $dom->find( 'img' );
+	
+	/**
+	 * Filter the max alt text length checked by the img_alt_long rule before it is considered an issue.
+	 *
+	 * @since 1.11.0
+	 *
+	 * @param int $length The length used in the rule to determine the max length before flagging as an issue.
+	 */
+	$max_alt_length = max( 1, absint( apply_filters( 'edac_max_alt_length', 300 ) ) );
 
 	foreach ( $images as $image ) {
 		if ( isset( $image ) && $image->hasAttribute( 'alt' ) && $image->getAttribute( 'alt' ) !== '' ) {
