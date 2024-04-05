bc39cef6d9e1f92bd4b1370769881dfc1ce35352
pattonwebz <will@pattonwebz.com>
2024-04-05 15:35:13 +0100

    Add an interface for data handling

---
+++ admin/data/interface-data.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Interface for data classes
+ *
+ * @since 1.11.0
+ *
+ * @package accessibility-checker
+ */
+
+namespace EDAC\Admin\Data;
+
+/**
+ * Interface for data classes
+ */
+interface Data_Interface {
+
+	/**
+	 * Get data
+	 *
+	 * @since 1.11.0
+	 *
+	 * @param string $key Optional. Key to get specific data.
+	 *
+	 * @return mixed
+	 */
+	public function get( string $key = '' );
+
+	/**
+	 * Save data
+	 *
+	 * @since 1.11.0
+	 *
+	 * @param mixed  $data Data to save.
+	 * @param string $key Optional. Key to save specific data.
+	 */
+	public function save( $data, string $key = '' );
+
+	/**
+	 * Delete data
+	 *
+	 * @since 1.11.0
+	 *
+	 * @param string $key Optional. Key to delete specific data.
+	 */
+	public function delete( string $key = '' );
+}
