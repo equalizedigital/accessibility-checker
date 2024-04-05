6b54c0798ffe86fb886ce79814beca47019ca650
pattonwebz <will@pattonwebz.com>
2024-04-05 21:33:40 +0100

    Add some methods to Summary_Data class to handle back compat

--- admin/data/post-meta/class-scan-summary.php
+++ admin/data/post-meta/class-scan-summary.php
@@ -116,14 +116,49 @@ class Scan_Summary implements Data_Interface {
 	 */
 	private function sanitize_summary( array $summary ): array {
 		return array(
-			'passed_tests'       => absint( $summary['passed_tests'] ?? 0 ),
-			'errors'             => absint( $summary['errors'] ?? 0 ),
-			'warnings'           => absint( $summary['warnings'] ?? 0 ),
-			'ignored'            => absint( $summary['ignored'] ?? 0 ),
-			'contrast_errors'    => absint( $summary['contrast_errors'] ?? 0 ),
-			'content_grade'      => absint( $summary['content_grade'] ?? 0 ),
-			'readability'        => sanitize_text_field( $summary['readability'] ?? '' ),
-			'simplified_summary' => filter_var( $summary['simplified_summary'] ?? false, FILTER_VALIDATE_BOOLEAN ),
+			'passed_tests'            => absint( $summary['passed_tests'] ?? 0 ),
+			'errors'                  => absint( $summary['errors'] ?? 0 ),
+			'warnings'                => absint( $summary['warnings'] ?? 0 ),
+			'ignored'                 => absint( $summary['ignored'] ?? 0 ),
+			'contrast_errors'         => absint( $summary['contrast_errors'] ?? 0 ),
+			'content_grade'           => absint( $summary['content_grade'] ?? 0 ),
+			'readability'             => sanitize_text_field( $summary['readability'] ?? '' ),
+			'simplified_summary'      => filter_var( // I would like to rename this to 'simplified_summary_required'.
+				$summary['simplified_summary'] ?? false,
+				FILTER_VALIDATE_BOOLEAN
+			),
+			'simplified_summary_text' => wp_kses_post( $summary['simplified_summary_text'] ?? '' ),
 		);
+	}
+
+	/**
+	 * For back compat with older data that was stored as individual post_meta values.
+	 *
+	 * This method should be removed after the next major release, or after a year, whichever comes first.
+	 *
+	 * @since 1.11.0
+	 *
+	 * @param string $key The key to get.
+	 *
+	 * @return mixed
+	 */
+	private function back_compat_get( string $key ) {
+		$maybe_back_compat_key = $this->get_key_for_back_compat( $key );
+		$meta_from_old_key     = get_post_meta( $this->post_id, $maybe_back_compat_key, true );
+		return $meta_from_old_key ?? '';
+	}
+
+	/**
+	 * Remap new keys to old keys to preserve back compat with older data.
+	 *
+	 * @param string $key - a key to check for a back compat equivalent.
+	 *
+	 * @return string - the back compat key if it exists, otherwise the original key.
+	 */
+	private function get_key_for_back_compat( string $key ): string {
+		$back_compat_keys = array(
+			'simplified_summary_text' => '_edac_simplified_summary',
+		);
+		return $back_compat_keys[ $key ] ?? $key;
 	}
 }
