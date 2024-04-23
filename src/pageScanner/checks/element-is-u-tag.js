23c898da2a4e29341da0991947b8961a3d8f91a3
pattonwebz <will@pattonwebz.com>
2024-04-23 19:50:15 +0100

    Add an axe-core formatted 'check' for elements that are `<u>` tags

---
+++ src/pageScanner/checks/element-is-u-tag.js
@@ -0,0 +1,13 @@
+/**
+ * Axe core check against nodes to determine if they are <u> tags.
+ *
+ * @param {Node} node The node to evaluate.
+ * @return {boolean} True if the node is a <u> tag, false otherwise.
+ */
+
+export default {
+	id: 'element_is_u_tag',
+	evaluate: ( node ) => {
+		return node.tagName.toLowerCase() === 'u';
+	},
+};
