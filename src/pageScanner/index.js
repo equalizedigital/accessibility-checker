33420b2e9c664091664a535ae1bb9779d4d4d613
pattonwebz <will@pattonwebz.com>
2024-05-22 14:14:23 +0100

    Add rule that checks for small text

--- src/pageScanner/index.js
+++ src/pageScanner/index.js
@@ -6,6 +6,8 @@ import colorContrastFailure from './rules/color-contrast-failure';
 import underlinedText from './rules/underlined-text';
 import elementWithUnderline from './checks/element-with-underline';
 import elementIsAUTag from './checks/element-is-u-tag';
+import textSmall from './rules/text-small';
+import textSizeTooSmall from './checks/text-size-too-small';
 
 //TODO: examples:
 //import customRule1 from './rules/custom-rule-1';
@@ -40,11 +42,13 @@ const scan = async (
 				// customRule1,
 				colorContrastFailure,
 				underlinedText,
+				textSmall,
 			],
 			checks: [
 				//alwaysFail,
 				elementIsAUTag,
 				elementWithUnderline,
+				textSizeTooSmall,
 			],
 			iframes: false,
 
@@ -56,6 +60,7 @@ const scan = async (
 				values: [
 					'color_contrast_failure',
 					'underlined_text',
+					textSmall.id,
 				],
 			},
 
---
+++ src/pageScanner/rules/text-small.js
@@ -0,0 +1,22 @@
+/**
+ * Rule: Text small failure
+ *
+ * Text should have a minimum size and anything below that is a fail.
+ */
+
+export default {
+	id: 'text_small',
+	impact: 'moderate',
+	selector: 'p, span, small, strong, b, i, h1, h2, h3, h4, h5, h6, a, label, button, th, td, li, div, blockquote, address, cite, code, pre, q, s, sub, sup, u, var, abbr, acronym, del, dfn, em, ins, kbd, samp, time, mark, meter, progress, ruby, rt, rp, wbr, bdi, bdo, br, hr, img, input, select, textarea, output, video, audio, canvas, map, area, object, embed, param, source, track, iframe, table, caption, col, colgroup, thead, tbody, tfoot, tr, th, td, dl, dt, dd, ol, ul, menu, dir, li, figure, figcaption, main, header, footer, nav, section, article, aside, details, dialog, summary, data, time, title, style, script, noscript, template',
+	matches: ( element ) => {
+		// only run checks on elements with text content
+		return element.textContent.trim().length;
+	},
+	tags: [ 'cat.text' ],
+	metadata: {
+		description: 'Text elements should not be too small.',
+	},
+	all: [],
+	any: [],
+	none: [ 'text_size_too_small' ],
+};
