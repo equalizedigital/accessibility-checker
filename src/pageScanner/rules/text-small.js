1fc87d214f94560592a44b1b777708bf8dea676b
pattonwebz <will@pattonwebz.com>
2024-05-23 19:56:25 +0100

    Narrow the tags text_small checks against slightly

--- src/pageScanner/rules/text-small.js
+++ src/pageScanner/rules/text-small.js
@@ -7,7 +7,7 @@
 export default {
 	id: 'text_small',
 	impact: 'moderate',
-	selector: 'p, span, small, strong, b, i, h1, h2, h3, h4, h5, h6, a, label, button, th, td, li, div, blockquote, address, cite, code, pre, q, s, sub, sup, u, var, abbr, acronym, del, dfn, em, ins, kbd, samp, time, mark, meter, progress, ruby, rt, rp, wbr, bdi, bdo, br, hr, img, input, select, textarea, output, video, audio, canvas, map, area, object, embed, param, source, track, iframe, table, caption, col, colgroup, thead, tbody, tfoot, tr, th, td, dl, dt, dd, ol, ul, menu, dir, li, figure, figcaption, main, header, footer, nav, section, article, aside, details, dialog, summary, data, time, title, style, script, noscript, template',
+	selector: 'p, span, small, strong, b, i, h1, h2, h3, h4, h5, h6, a, label, button, th, td, li, div, blockquote, address, cite, code, pre, q, s, sub, sup, u, var, abbr, acronym, del, dfn, em, ins, kbd, input, select, textarea, caption, dl, dt, dd, li, figure, figcaption, details, dialog, summary, data, time',
 	matches: ( element ) => {
 		// only run checks on elements with text content
 		return element.textContent.trim().length;
