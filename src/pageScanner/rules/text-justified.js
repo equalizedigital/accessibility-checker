497d90d23cd9279a0a8a339a21ff54608060a6cb
pattonwebz <will@pattonwebz.com>
2024-05-23 16:26:25 +0100

    Use a wider group of selectors for text_justified check

--- src/pageScanner/rules/text-justified.js
+++ src/pageScanner/rules/text-justified.js
@@ -7,7 +7,7 @@
 const TEXT_JUSTIFIED_CHECK_THRESHOLD = 200;
 export default {
 	id: 'text_justified',
-	selector: 'p, div, td',
+	selector: 'p, span, small, strong, b, i, em, h1, h2, h3, h4, h5, h6, a, label, button, th, td, li, div, blockquote, address, cite, q, s, sub, sup, u, del, caption, dt, dd, figcaption, summary, data, time',
 	matches: ( element ) => {
 		return element.textContent.trim().length >= TEXT_JUSTIFIED_CHECK_THRESHOLD;
 	},
