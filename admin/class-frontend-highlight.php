141fea81945ad1cda030ac9e27fc23112c7e0717
William Patton <will@pattonwebz.com>
2024-06-21 18:44:00 +0100

    Update the `since` tag with 1.4.0

--- admin/class-frontend-highlight.php
+++ admin/class-frontend-highlight.php
@@ -36,7 +36,7 @@ class Frontend_Highlight {
 		 * highlighter. You can use the filter to perform additional permission checks
 		 * on who can see it.
 		 *
-		 * @since 1.x.x
+		 * @since 1.4.0
 		 *
 		 * @param bool $visibility The visibility of the frontend highlighter. Default is false, return true to show the frontend highlighter.
 		 */
