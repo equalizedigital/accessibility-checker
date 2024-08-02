b0e68c4b7ff992a9d6e5de9538a19108978a17d9
pattonwebz <will@pattonwebz.com>
2024-08-02 15:36:03 +0100

    Add a fixes page class and a partial to render it's settings on it's page

---
+++ partials/admin-page/fixes-page.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Accessibility Checker pluign file.
+ *
+ * @package Accessibility_Checker
+ */
+
+?>
+
+<div class="edac-settings-general <?php echo EDAC_KEY_VALID ? '' : 'edac-show-pro-callout'; ?>">
+	<form action="options.php" method="post">
+		<?php
+		settings_fields( 'edac_settings_fixes' );
+		do_settings_sections( 'edac_settings_fixes' );
+		submit_button();
+		?>
+	</form>
+	<?php if ( EDAC_KEY_VALID === false ) { ?>
+		<div><?php include 'pro-callout.php'; ?></div>
+	<?php } ?>
+</div>
