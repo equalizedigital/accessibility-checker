bf355fb63c7096f6641f4ccab07477abb7cd3783
pattonwebz <will@pattonwebz.com>
2024-08-05 21:44:23 +0100

    Add a FixesManager class to handle the necessary setup needed on each new fix being added

---
+++ includes/classes/Fixes/FixesManager.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Manager class for fixes.
+ *
+ * @package Accessibility_Checker
+ */
+
+namespace EqualizeDigital\AccessibilityChecker\Fixes;
+
+/**
+ * Manager class for fixes.
+ *
+ * @since 1.16.0
+ */
+class FixesManager {
+
+	/**
+	 * The single instance of the class.
+	 *
+	 * @var FixesManager|null
+	 */
+	private static $instance = null;
+
+	/**
+	 * The fixes.
+	 *
+	 * @var array
+	 */
+	private $fixes = [];
+
+	/**
+	 * Private constructor to prevent direct instantiation.
+	 */
+	private function __construct() {
+		// stub.
+	}
+
+	/**
+	 * Get the single instance of the class.
+	 *
+	 * @return FixesManager
+	 */
+	public static function get_instance() {
+		if ( null === self::$instance ) {
+			self::$instance = new self();
+		}
+		return self::$instance;
+	}
+
+	/**
+	 * Load the fixes.
+	 */
+	private function load_fixes() {
+		$fixes = apply_filters( 'edac_filter_fixes', [] );
+		foreach ( $fixes as $fix ) {
+			if ( is_subclass_of( $fix, '\EqualizeDigital\AccessibilityChecker\Fixes\FixInterface' ) ) {
+				if ( ! isset( $this->fixes[ $fix_class::get_slug() ] ) ) {
+					$fix_class = new $fix();
+					$fix_class->register();
+					$this->fixes[ $fix_class::get_slug() ] = $fix_class;
+				}
+			}
+		}
+	}
+
+	/**
+	 * Get a fix by its slug.
+	 *
+	 * @param string $slug The fix slug.
+	 *
+	 * @return FixInterface|null
+	 */
+	public function get_fix( $slug ) {
+		return isset( $this->fixes[ $slug ] ) ? $this->fixes[ $slug ] : null;
+	}
+
+	/**
+	 * Register the fixes.
+	 */
+	public function register_fixes() {
+		$this->load_fixes();
+
+		foreach ( $this->fixes as $fix ) {
+			if ( 'backend' === $fix::get_type() && is_admin() ) {
+				$fix->register();
+			} elseif ( 'frontend' === $fix::get_type() && ! is_admin() ) {
+				$fix->register();
+			} elseif ( 'everywhere' === $fix::get_type() ) {
+				$fix->register();
+			}
+		}
+	}
+}
