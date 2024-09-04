78126182f5b73ada3c6a9150e613b6d3282a2efb
Steve Jones <steve@stevejonesdev.com>
2024-09-04 10:57:43 -0400

    fixed: typo

--- src/frontendFixes/index.js
+++ src/frontendFixes/index.js
@@ -1,21 +1,21 @@
 const edacFrontendFixes = window.edac_frontend_fixes || {};
 
 if ( edacFrontendFixes.skip_link.enabled ) {
-	// lazy inport the module
+	// lazy import the module
 	import( /* webpackChunkName: "skip-link" */ './Fixes/skipLinkFix' ).then( ( skipLinkFix ) => {
 		skipLinkFix.default();
 	} );
 }
 
 if ( edacFrontendFixes.lang_and_dir.enabled ) {
-	// lazy inport the module
+	// lazy import the module
 	import( /* webpackChunkName: "aria-hidden" */ './Fixes/langAndDirFix' ).then( ( langAndDirFix ) => {
 		langAndDirFix.default();
 	} );
 }
 
 if ( edacFrontendFixes.tabindex.enabled ) {
-	// lazy inport the module
+	// lazy import the module
 	import( /* webpackChunkName: "tabindex" */ './Fixes/tabindexFix' ).then( ( tabindexFix ) => {
 		tabindexFix.default();
 	} );
