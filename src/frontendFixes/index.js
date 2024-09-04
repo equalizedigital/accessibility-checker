b9972f933f392028975d61f70d0374c340d2ba30
pattonwebz <will@pattonwebz.com>
2024-09-04 21:11:45 +0100

    Add optional chaining to the fixes enabled condition checks

--- src/frontendFixes/index.js
+++ src/frontendFixes/index.js
@@ -1,20 +1,20 @@
 const edacFrontendFixes = window.edac_frontend_fixes || {};
 
-if ( edacFrontendFixes.skip_link.enabled ) {
+if ( edacFrontendFixes?.skip_link?.enabled ) {
 	// lazy import the module
 	import( /* webpackChunkName: "skip-link" */ './Fixes/skipLinkFix' ).then( ( skipLinkFix ) => {
 		skipLinkFix.default();
 	} );
 }
 
-if ( edacFrontendFixes.lang_and_dir.enabled ) {
+if ( edacFrontendFixes?.lang_and_dir?.enabled ) {
 	// lazy import the module
 	import( /* webpackChunkName: "aria-hidden" */ './Fixes/langAndDirFix' ).then( ( langAndDirFix ) => {
 		langAndDirFix.default();
 	} );
 }
 
-if ( edacFrontendFixes.tabindex.enabled ) {
+if ( edacFrontendFixes?.tabindex?.enabled ) {
 	// lazy import the module
 	import( /* webpackChunkName: "tabindex" */ './Fixes/tabindexFix' ).then( ( tabindexFix ) => {
 		tabindexFix.default();
