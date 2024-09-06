a93a6dc0047555f85687586d2f098a1309cf99b7
Steve Jones <steve@stevejonesdev.com>
2024-09-06 16:35:13 -0400

    updated: underline fix logic

--- src/frontendFixes/Fixes/underlineFix.js
+++ src/frontendFixes/Fixes/underlineFix.js
@@ -10,15 +10,18 @@ const ForceUnderlineFix = () => {
 	// Apply underline to any link that is not within a `nav` element.
 	// Using JavaScript ensures this style takes precedence over conflicting CSS rules.
 	const targets = document.querySelectorAll( ForceUnderlineFixData.target );
-	const originalOutline = targets[ 0 ].style.outlineWidth;
-	const originalOffset = targets[ 0 ].style.outlineOffset;
-	const textColor = targets[ 0 ].style.color;
-	const originalColor = targets[ 0 ].style.outlineColor;
 
 	targets.forEach( function( target ) {
 		if ( ! target.closest( 'nav' ) ) {
 			target.style.textDecoration = 'underline';
 		}
+
+		// Store the original styles in data-* attributes
+		target.setAttribute( 'data-original-outline', target.style.outlineWidth );
+		target.setAttribute( 'data-original-offset', target.style.outlineOffset );
+		target.setAttribute( 'data-original-color', target.style.outlineColor );
+
+		const textColor = target.style.color; // Capture the text color separately
 
 		target.addEventListener( 'mouseenter', function() {
 			target.style.textDecoration = 'none';
@@ -30,7 +33,7 @@ const ForceUnderlineFix = () => {
 
 		target.addEventListener( 'focusin', function() {
 			let newOutline = '2px';
-			if ( originalOutline === '2px' ) {
+			if ( target.style.outlineWidth === '2px' ) {
 				newOutline = '4px';
 			}
 			// Increase outline thickness and adjust color and offset to ensure a visible focus indicator.
@@ -40,9 +43,10 @@ const ForceUnderlineFix = () => {
 		} );
 
 		target.addEventListener( 'focusout', function() {
-			target.style.outlineWidth = originalOutline;
-			target.style.outlineColor = originalColor;
-			target.style.outlineOffset = originalOffset;
+			// Restore the original styles from data-* attributes
+			target.style.outlineWidth = target.getAttribute( 'data-original-outline' );
+			target.style.outlineColor = target.getAttribute( 'data-original-color' );
+			target.style.outlineOffset = target.getAttribute( 'data-original-offset' );
 		} );
 	} );
 };
