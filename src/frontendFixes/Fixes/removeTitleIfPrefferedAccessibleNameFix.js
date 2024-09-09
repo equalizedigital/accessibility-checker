af43e921a84a40cd55bc1fcb13beb2b8b931084d
pattonwebz <will@pattonwebz.com>
2024-09-09 22:55:12 +0100

    Refactor to prevent running multiple queries and multiple loops

--- src/frontendFixes/Fixes/removeTitleIfPrefferedAccessibleNameFix.js
+++ src/frontendFixes/Fixes/removeTitleIfPrefferedAccessibleNameFix.js
@@ -9,69 +9,91 @@ const RemoveTitleIfPreferredAccessibleName = () => {
 
 	const elementsWithTitle = document.querySelectorAll( 'img[title], a[title], input[title], textarea[title], select[title], button[title]' );
 
-	const imagesWithTitles = document.querySelectorAll( 'img[title]' );
-	imagesWithTitles.forEach( ( element ) => {
+	elementsWithTitle.forEach( ( element ) => {
 		if ( element.getAttribute( 'title' )?.trim() === '' ) {
 			return;
 		}
 
-		// if the image has a non-empty alt attribute, remove the title attribute
-		if ( element.getAttribute( 'alt' )?.trim() !== '' ) {
-			removeTitle( element );
-			return;
+		const tagName = element.tagName.toLowerCase();
+
+		if ( tagName === 'img' ) {
+			handleImageElement( element );
+		} else if ( [ 'a', 'button' ].includes( tagName ) ) {
+			handleLinkOrButtonElement( element );
+		} else if ( [ 'input', 'textarea', 'select' ].includes( tagName ) ) {
+			handleInputElements( element );
 		}
-		// since the alt is empty put the title in the alt then remove the title.
-		element.setAttribute( 'alt', element.getAttribute( 'title' ) );
-		removeTitle( element );
 	} );
+};
 
-	const linksOrButtonsWithTitles = elementsWithTitle.querySelectorAll( 'a, button' );
-	linksOrButtonsWithTitles.forEach( ( element ) => {
-		if ( element.getAttribute( 'title' )?.trim() === '' ) {
-			return;
-		}
-
-		// if the element has non-empty text content, remove the title attribute
-		if ( element.innerText.trim() !== '' ) {
-			removeTitle( element );
-			return;
-		}
-
-		// element has no text content, check if it has aria-label or aria-labelledby attribute
-		const ariaLabel = element.getAttribute( 'aria-label' );
-		const ariaLabelledBy = element.getAttribute( 'aria-labelledby' );
-		if ( ariaLabel?.trim() !== '' || ariaLabelledBy?.trim() !== '' ) {
-			removeTitle( element );
-			return;
-		}
-
-		// by this point the element has no text content, aria-label or aria-labelledby, move the title to the aria-label then remove the title
-		element.setAttribute( 'aria-label', element.getAttribute( 'title' ) );
+/**
+ * Handle image elements.
+ *
+ * Prefer alt attribute over title attribute.
+ *
+ * @param {HTMLElement} element
+ */
+const handleImageElement = ( element ) => {
+	// if the image has a non-empty alt attribute, remove the title attribute
+	if ( element.getAttribute( 'alt' )?.trim() !== '' ) {
 		removeTitle( element );
-	} );
+		return;
+	}
+	// since the alt is empty put the title in the alt then remove the title.
+	element.setAttribute( 'alt', element.getAttribute( 'title' ) );
+	removeTitle( element );
+};
 
-	const inputsWithTitles = elementsWithTitle.querySelectorAll( 'input, textarea, select' );
-	inputsWithTitles.forEach( ( element ) => {
-		if ( element.getAttribute( 'title' )?.trim() === '' ) {
-			return;
-		}
-
-		// check for some associated label or wrapping label.
-		const ariaLabel = element.getAttribute( 'aria-label' );
-		const ariaLabelledBy = element.getAttribute( 'aria-labelledby' );
-		const associatedLabel = element.labels?.[ 0 ]?.innerText;
-		const wrappingLabel = element.closest( 'label' )?.innerText;
-
-		if ( ariaLabel?.trim() !== '' || ariaLabelledBy?.trim() !== '' || associatedLabel?.trim() !== '' || wrappingLabel?.trim() !== '' ) {
-			// there exists a label already, remove the title attribute.
-			removeTitle( element );
-			return;
-		}
-
-		// by this point there is no proper labeling found so move the title to the aria-label then remove the title
-		element.setAttribute( 'aria-label', element.getAttribute( 'title' ) );
+/**
+ * Handle link or button elements.
+ *
+ * Prefer aria-label or aria-labelledby over title attribute.
+ *
+ * @param {HTMLElement} element
+ */
+const handleLinkOrButtonElement = ( element ) => {
+	// if the element has non-empty text content, remove the title attribute
+	if ( element.innerText.trim() !== '' ) {
 		removeTitle( element );
-	} );
+		return;
+	}
+
+	// element has no text content, check if it has aria-label or aria-labelledby attribute
+	const ariaLabel = element.getAttribute( 'aria-label' );
+	const ariaLabelledBy = element.getAttribute( 'aria-labelledby' );
+	if ( ariaLabel?.trim() !== '' || ariaLabelledBy?.trim() !== '' ) {
+		removeTitle( element );
+		return;
+	}
+
+	// by this point the element has no text content, aria-label or aria-labelledby, move the title to the aria-label then remove the title
+	element.setAttribute( 'aria-label', element.getAttribute( 'title' ) );
+	removeTitle( element );
+};
+
+/**
+ * Handle input elements.
+ *
+ * Prefer aria-label or aria-labelledby over title attribute.
+ *
+ * @param {HTMLElement} element
+ */
+const handleInputElements = ( element ) => {
+	// check for some associated label or wrapping label.
+	const ariaLabel = element.getAttribute( 'aria-label' );
+	const ariaLabelledBy = element.getAttribute( 'aria-labelledby' );
+	const associatedLabel = element.labels?.[ 0 ]?.innerText;
+	const wrappingLabel = element.closest( 'label' )?.innerText;
+
+	if ( ariaLabel?.trim() !== '' || ariaLabelledBy?.trim() !== '' || associatedLabel?.trim() !== '' || wrappingLabel?.trim() !== '' ) {
+		// there exists a label already, remove the title attribute.
+		removeTitle( element );
+		return;
+	}
+
+	// by this point there is no proper labeling found so move the title to the aria-label then remove the title
+	element.setAttribute( 'aria-label', element.getAttribute( 'title' ) );
+	removeTitle( element );
 };
 
 const removeTitle = ( element ) => {
