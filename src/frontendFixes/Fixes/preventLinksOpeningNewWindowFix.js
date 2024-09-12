cde96e8dc91954f893959a863a7a9045b8b7047f
pattonwebz <will@pattonwebz.com>
2024-09-12 21:17:09 +0100

    Allow bypass of the target="_blank" removal by using the `.allow-new-tab` class

--- src/frontendFixes/Fixes/preventLinksOpeningNewWindowFix.js
+++ src/frontendFixes/Fixes/preventLinksOpeningNewWindowFix.js
@@ -1,6 +1,10 @@
 const preventLinksOpeningNewWindowFix = () => {
-	const links = document.querySelectorAll( 'a[target="_blank"]' );
+	const links = document.querySelectorAll( 'a[target="_blank"]:not(.allow-new-tab)' );
 	links.forEach( ( link ) => {
+		// If the link is in a container that allows new tabs, don't remove the target attribute.
+		if ( link.closest( '.allow-new-tab' ) ) {
+			return;
+		}
 		link.removeAttribute( 'target' );
 		link.classList.add( 'edac-removed-target-blank' );
 	} );
