dfb57e516d66dc5389a2d86b229964058a3d613b
pattonwebz <will@pattonwebz.com>
2024-09-27 16:54:15 +0100

    Process fix updates from frontend highlighter in grouped batches

--- src/frontendHighlighterApp/index.js
+++ src/frontendHighlighterApp/index.js
@@ -795,8 +795,25 @@ class AccessibilityCheckerHighlight {
 
 		const fixSettingsContainer = event.target.closest( '.edac-highlight-panel-description-fix-settings' );
 
-		fixSettingsContainer.querySelectorAll( 'input, select, textarea' ).forEach( ( input ) => {
-			settingsToSave[ input.name ] = input.value;
+		fixSettingsContainer.querySelectorAll( 'input, select, textarea' ).forEach( ( field ) => {
+			const fixGroup = field.getAttribute( 'data-fix-slug' );
+			if ( ! fixGroup ) {
+				// a group is required to save.
+				return;
+			}
+			if ( settingsToSave[ fixGroup ] === undefined ) {
+				settingsToSave[ fixGroup ] = {};
+			}
+
+			// Value to save for checkboxes differs to other field types.
+			switch ( field.type ) {
+				case 'checkbox':
+					settingsToSave[ fixGroup ][ field.name ] = field.checked;
+					break;
+
+				default:
+					settingsToSave[ fixGroup ][ field.name ] = field.value;
+			}
 		} );
 
 		fixSettingsContainer.classList.add( 'edac-highlight-panel-description-fix-settings--saving' );
@@ -808,7 +825,17 @@ class AccessibilityCheckerHighlight {
 				'Content-Type': 'application/json',
 			},
 			body: JSON.stringify( settingsToSave ),
-		} );
+		} ).then(
+			( response ) => {
+				fixSettingsContainer.classList.remove( 'edac-highlight-panel-description-fix-settings--saving' );
+				if ( response.ok ) {
+					fixSettingsContainer.classList.remove( 'edac-highlight-panel-description-fix-settings--error' );
+					fixSettingsContainer.classList.add( 'edac-highlight-panel-description-fix-settings--saved' );
+				} else {
+					fixSettingsContainer.classList.add( 'edac-highlight-panel-description-fix-settings--error' );
+				}
+			}
+		);
 	}
 
 	/**
