5b2ebd8dfec393c2bf090551657edb4ef6867a4d
pattonwebz <will@pattonwebz.com>
2024-10-08 15:34:20 +0100

    Add tags for the link_blank rule

--- src/pageScanner/rules/link_blank.js
+++ src/pageScanner/rules/link_blank.js
@@ -6,7 +6,12 @@ export default {
 	id: 'link_blank',
 	selector: 'a[target="_blank"]',
 	excludeHidden: false,
-	tags: [],
+	tags: [
+		'cat.custom',
+		'wcag2aaa',
+		'wcag322',
+		'wcag325',
+	],
 	metadata: {
 		description: 'Links that open in a new tab should inform the user.',
 		help: 'Links that open in a new tab should inform the user. This is important for users who rely on screen readers, as they may not realize that a new tab has opened.',
