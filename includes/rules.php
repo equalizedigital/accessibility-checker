ad8822b2dad7cbee0000849827d7bf362570caca
pattonwebz <will@pattonwebz.com>
2024-11-01 17:46:59 +0000

    Remove the empty_form_label rule definition
    This was intended to be removed but was re-added in merge resolution here https://github.com/equalizedigital/accessibility-checker/commit/0b6f3508eac7163f37e4b458730dbe1c5cdd9fca#diff-40652b9b45019a70bc2344f1b523a3220a53ef38d2d326f6821588666a1d2e7a

--- includes/rules.php
+++ includes/rules.php
@@ -270,23 +270,6 @@ return [
 		),
 	],
 	[
-		'title'     => esc_html__( 'Empty Form Label', 'accessibility-checker' ),
-		'info_url'  => 'https://a11ychecker.com/help4109',
-		'slug'      => 'empty_form_label',
-		'rule_type' => 'error',
-		'summary'   => sprintf(
-			// translators: %s is <code>&lt;label&gt;</code>.
-			esc_html__( 'An Empty Form Label error is triggered when a %s tag is present in your form and associated with an input (form field), but does not contain any text. To fix empty form label errors, you will need to determine how the field and form were created and then add text to the label for the field that is currently blank.', 'accessibility-checker' ),
-			'<code>&lt;label&gt;</code>'
-		),
-		'ruleset'   => 'js',
-		'combines'  => [ 'label' ],
-		'fixes'     => [
-			AddLabelToUnlabelledFormFieldsFix::get_slug(),
-			CommentSearchLabelFix::get_slug(),
-		],
-	],
-	[
 		'title'     => esc_html__( 'Missing Form Label', 'accessibility-checker' ),
 		'info_url'  => 'https://a11ychecker.com/help1949',
 		'slug'      => 'missing_form_label',
