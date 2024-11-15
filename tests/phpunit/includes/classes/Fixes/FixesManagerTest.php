09163fa76a2a866d0ecdceea379e6da561c086c3
pattonwebz <will@pattonwebz.com>
2024-11-15 19:12:27 +0000

    Use Mockery for mocking the Fixes_Manager
    This allows mocking a static method

--- tests/phpunit/includes/classes/Fixes/FixesManagerTest.php
+++ tests/phpunit/includes/classes/Fixes/FixesManagerTest.php
@@ -5,6 +5,7 @@
  * @package accessibility-checker
  */
 
+use Mockery;
 use PHPUnit\Framework\TestCase;
 use EqualizeDigital\AccessibilityChecker\Fixes\FixesManager;
 use EqualizeDigital\AccessibilityChecker\Fixes\FixInterface;
@@ -29,6 +30,15 @@ class FixesManagerTest extends TestCase {
 	}
 
 	/**
+	 * Tear down the test environment by closing Mockery.
+	 *
+	 * @return void
+	 */
+	public function tearDown(): void {
+		Mockery::close();
+	}
+
+	/**
 	 * Test that the instance retuns an empty array when no fixes are registered.
 	 *
 	 * @return void
@@ -44,14 +54,14 @@ class FixesManagerTest extends TestCase {
 	 * @return void
 	 */
 	public function test_get_fixes_settings_returns_correct_structure() {
-		$fix_mock = $this->createMock( FixInterface::class );
-		$fix_mock->method( 'get_fields_array' )->willReturn(
+		$fix_mock = Mockery::mock( 'alias:EqualizeDigital\AccessibilityChecker\Fixes\Fix\AddFileSizeAndTypeToLinkedFilesFix' );
+		$fix_mock->shouldReceive( 'get_slug' )->andReturn( 'mock_fix' );
+		$fix_mock->shouldReceive( 'get_fields_array' )->andReturn(
 			[
 				'field1' => [ 'default' => 'value1' ],
 				'field2' => [ 'default' => 'value2' ],
 			]
 		);
-		$fix_mock->method( 'get_slug' )->willReturn( 'mock_fix' );
 		$fix_mock->is_pro = true;
 
 		$fixes_manager  = FixesManager::get_instance();
@@ -79,14 +89,14 @@ class FixesManagerTest extends TestCase {
 	 * @return void
 	 */
 	public function test_get_fixes_settings_uses_default_values() {
-		$fix_mock = $this->createMock( FixInterface::class );
-		$fix_mock->method( 'get_fields_array' )->willReturn(
+		$fix_mock = Mockery::mock( 'alias:EqualizeDigital\AccessibilityChecker\Fixes\Fix\AddFileSizeAndTypeToLinkedFilesFix' );
+		$fix_mock->shouldReceive( 'get_slug' )->andReturn( 'mock_fix' );
+		$fix_mock->shouldReceive( 'get_fields_array' )->andReturn(
 			[
 				'field1' => [ 'default' => 'default_value1' ],
 				'field2' => [ 'default' => 'default_value2' ],
 			]
 		);
-		$fix_mock->method( 'get_slug' )->willReturn( 'mock_fix' );
 
 		$fixes_manager  = FixesManager::get_instance();
 		$reflection     = new ReflectionClass( $fixes_manager );
