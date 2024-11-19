7b0b89632b89756a80552b11ca7c2ccb124937ca
William Patton <will@pattonwebz.com>
2024-11-19 17:15:46 +0000

    Update fix settings save message
    Co-authored-by: Steve Jones <steve@stevejonesdev.com>

--- partials/admin-page/fixes-page.php
+++ partials/admin-page/fixes-page.php
@@ -9,7 +9,7 @@
 <div id="edac-fixes-page" class="wrap edac-settings <?php echo EDAC_KEY_VALID ? '' : 'pro-callout-wrapper'; ?>">
 	<?php if ( get_transient( 'edac_fixes_settings_saved' ) ) : ?>
 		<div class="notice notice-warning is-dismissible">
-			<p><?php esc_html_e( 'Settings saved. You need to rescan your site for the updates to be confirmed and reflected in site stats. If you have a caching plugin on your site make sure to flush is so that the updated fixes are reflected.', 'accessibility-checker' ); ?></p>
+			<p><?php esc_html_e( 'Settings Saved. To confirm and reflect these updates in your site's statistics, please rescan your site. If you are using a caching plugin, make sure to clear the cache to ensure the updated fixes are applied correctly.', 'accessibility-checker' ); ?></p>
 		</div>
 		<?php
 		delete_transient( 'edac_fixes_settings_saved' );
