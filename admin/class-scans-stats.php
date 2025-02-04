bd062beffdc223e7efaf4de9ba8385001ac05800
pattonwebz <will@pattonwebz.com>
2025-02-04 00:52:24 +0000

    Don't show < 1% for average density - use real value even under 1%

--- admin/class-scans-stats.php
+++ admin/class-scans-stats.php
@@ -306,7 +306,7 @@ class Scans_Stats {
 		if ( null === $data['avg_issue_density_percentage'] ) {
 			$data['avg_issue_density_percentage'] = 'N/A';
 		} else {
-			$data['avg_issue_density_percentage'] = $data['avg_issue_density_percentage'] < 1 ? '< 1%' : round( $data['avg_issue_density_percentage'], 2 );
+			$data['avg_issue_density_percentage'] = round( $data['avg_issue_density_percentage'], 2 );
 		}
 
 		$data['fullscan_running']      = false;
