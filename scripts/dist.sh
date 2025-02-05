73cb67dfe80a3df6c19db1f17b21700c03575fb8
pattonwebz <will@pattonwebz.com>
2025-02-05 19:01:10 +0000

    Update some comments in dist script

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -20,37 +20,36 @@ while getopts ":-:" opt; do
   esac
 done
 
-# Ensure ./dist directory exists.
-#rm -frd ./dist
+# Ensure ./dist directory exists, if not create it
 mkdir -p ./dist
 
-# Run the wp script that produces the zip
+# Run the wp-scripts command that produces the zip
 npx wp-scripts plugin-zip
 
-# Always clear the dist/accessiblity-checker folder before unzipping
+# Always clear the dist/accessibility-checker folder before unzipping
 rm -rfd ./dist/accessibility-checker
 
-# unzip the zip into its own folder so we can zip that
+# Unzip the zip into its own folder so we can repackage with some changes
 unzip accessibility-checker.zip -d ./dist/accessibility-checker
 
-# plugin-zip includes package.json which is not needed for the plugin, so remove.
+# The plugin-zip commands includes package.json, which is not needed for the plugin, so remove it and the repo README
 rm ./dist/accessibility-checker/package.json
 rm ./dist/accessibility-checker/README.md
 
-# remove the unneeded (almost 1MB) tests folder for textstatistics package
+# Remove the unneeded (almost 1MB) tests folder for textstatistics package
 rm ./dist/accessibility-checker/vendor/davechild/textstatistics/tests -r
 
-#remove the original zip
+# Remove the original zip
 rm accessibility-checker.zip
 
-# get the digits at the end of the line starting with ' * Version:' from the main plugin file
+# Get the string at the end of the line starting with ' * Version:' from the main plugin file
 VERSION=$(grep " * Version:" ./dist/accessibility-checker/accessibility-checker.php | grep -o '[0-9.]*\(-[a-zA-Z0-9.]*\)*')
 
 # Move into the dist folder and zip the plugin's folder
 cd ./dist
 zip -r accessibility-checker-$VERSION.zip accessibility-checker
 
-#cleanup and drop back into the original dir
+# Drop back into the original dir
 cd ..
 
 # Skip this step if the 'keep-build-folder' flag is true
