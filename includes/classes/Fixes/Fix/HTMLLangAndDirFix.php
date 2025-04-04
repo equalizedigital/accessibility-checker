897e79f142105db235e7ed4ecd8fcbeea3ea8c4a
pattonwebz <will@pattonwebz.com>
2025-04-04 19:44:33 +0100

    Fix a labelledby reference

--- includes/classes/Fixes/Fix/HTMLLangAndDirFix.php
+++ includes/classes/Fixes/Fix/HTMLLangAndDirFix.php
@@ -74,7 +74,7 @@ class HTMLLangAndDirFix implements FixInterface {
 		$fields['edac_fix_add_lang_and_dir'] = [
 			'type'        => 'checkbox',
 			'label'       => esc_html__( 'Add "lang" & "dir" Attributes', 'accessibility-checker' ),
-			'labelledby'  => 'add_read_more_title',
+			'labelledby'  => 'add_lang_and_dir',
 			// translators: %1$s: a attribute name wrapped in a <code> tag. %2$s: dir attribute %3$s: html element.
 			'description' => sprintf( __( 'Add the site language (%1$s) and text direction (%2$s) attributes to the %3$s element.', 'accessibility-checker' ), '<code>lang</code>', '<code>dir</code>', '<code>&lt;html&gt;</code>' ),
 			'fix_slug'    => $this->get_slug(),
