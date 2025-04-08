98c25217e3a3a4b7519818b04a0e1a86442d88e5
pattonwebz <will@pattonwebz.com>
2025-04-08 18:26:19 +0100

    Add a description to the aria_broken_reference rule

--- src/pageScanner/rules/aria-broken-reference.js
+++ src/pageScanner/rules/aria-broken-reference.js
@@ -6,9 +6,9 @@ export default {
 
 	],
 	metadata: {
-		description: '',
-		help: '',
-		impact: '',
+		description: 'Ensures ARIA attributes reference existing elements',
+		help: 'ARIA attributes that reference other elements must point to elements that exist in the DOM',
+		impact: 'critical',
 	},
 	all: [],
 	any: [ 'aria_label_not_found', 'aria_describedby_not_found', 'aria_owns_not_found' ],
