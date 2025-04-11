a935bfcd7c64593fbf31a99df6ca1f534c9c86fa
pattonwebz <will@pattonwebz.com>
2025-04-11 19:49:13 +0100

    Add img_alt_empty to JS scanner and remove the php rule
    [PRO-47]

--- src/pageScanner/index.js
+++ src/pageScanner/index.js
@@ -40,6 +40,8 @@ import isvideoDetected from './checks/is-video-detected';
 import videoPresent from './rules/video-present';
 import emptyTableHeader from './rules/empty-table-header';
 import tableHeaderIsEmpty from './checks/table-header-is-empty';
+import imgAltEmpty from './rules/img-alt-empty';
+import imgAltEmptyCheck from './checks/img-alt-empty-check';
 
 //TODO: examples:
 //import customRule1 from './rules/custom-rule-1';
@@ -92,6 +94,7 @@ const scan = async (
 				sliderPresent,
 				videoPresent,
 				emptyTableHeader,
+				imgAltEmpty,
 			],
 			checks: [
 				alwaysFail,
@@ -113,6 +116,7 @@ const scan = async (
 				sliderDetected,
 				isvideoDetected,
 				tableHeaderIsEmpty,
+				imgAltEmptyCheck,
 			],
 			iframes: false,
 
@@ -150,6 +154,7 @@ const scan = async (
 					sliderPresent.id,
 					videoPresent.id,
 					emptyTableHeader.id,
+					imgAltEmpty.id,
 				],
 			},
 
