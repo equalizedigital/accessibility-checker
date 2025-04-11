a1c9bec6d4d6936e960c45db788294437608f0c7
pattonwebz <will@pattonwebz.com>
2025-04-11 16:52:32 +0100

    Add a helper to track visibility of a node
    [PRO-45]

--- src/pageScanner/helpers/helpers.js
+++ src/pageScanner/helpers/helpers.js
@@ -6,3 +6,26 @@ export const fontSizeInPx = ( node ) => {
 	const fontSize = parseFloat( window.getComputedStyle( node ).fontSize );
 	return typeof fontSize === 'number' ? fontSize : 0;
 };
+
+/**
+ * Check if an element is visible in the DOM
+ *
+ * This is a recursive function so could be inefficient for deeply nested elements.
+ *
+ * @param {element} element an element to check for visibility.
+ * @return {boolean|boolean|*} A boolean indicating if the element is visible or not.
+ */
+export const isElementVisible = ( element ) => {
+	if ( ! element ) {
+		return false;
+	} // If the element doesn't exist
+	const style = window.getComputedStyle( element );
+
+	// Check if the element itself is hidden
+	if ( style.display === 'none' || style.visibility === 'hidden' || style.opacity === '0' ) {
+		return false;
+	}
+
+	// Check the parent recursively
+	return element.parentElement ? isElementVisible( element.parentElement ) : true;
+};
