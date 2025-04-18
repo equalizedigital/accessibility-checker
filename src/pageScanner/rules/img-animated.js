fbeee5c83f7fd6282c4d249cb3f917c621a24d79
pattonwebz <will@pattonwebz.com>
2025-04-18 13:56:55 +0100

    Update rule file comment
    [PRO-64]

--- src/pageScanner/rules/img-animated.js
+++ src/pageScanner/rules/img-animated.js
@@ -1,12 +1,6 @@
 /**
- * Rule to detect animated content that may cause accessibility issues
- *
- * This implements WCAG 2.2.2 (Pause, Stop, Hide) which requires that:
- * - Moving, blinking, or scrolling content that starts automatically and lasts more than 5 seconds
- *   must have a mechanism to pause, stop, or hide it
- * - Auto-updating content must have a mechanism to pause, stop, hide, or control the frequency
+ * Detect animated gifs and webps as well as common animated gif services.
  */
-
 export default {
 	id: 'img_animated',
 	// Selects both images and iframes that might contain animations
