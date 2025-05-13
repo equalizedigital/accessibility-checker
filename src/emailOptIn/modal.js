527dc0edd36668c9bcd6c12d3a6a9a1be27bb0cc
pattonwebz <will@pattonwebz.com>
2025-05-13 18:24:25 +0100

    Add a bail for if the modal stays empty to avoid locking people in the thickbox overlay

--- src/emailOptIn/modal.js
+++ src/emailOptIn/modal.js
@@ -29,10 +29,10 @@ const triggerModal = ( () => {
 
 		tb_show( 'Accessibility Checker', '#TB_inline?width=600&inlineId=edac-opt-in-modal', null );
 
-		// create a loop that will wait to find the close button before trying to bind the focus trap
+		// Loop and check for the close button before trying to bind the focus trap.
 		let attempts = 0;
 		const intervalId = setInterval( () => {
-			if ( bindFocusTrap() || attempts >= 10 ) {
+			if ( bindFocusTrap() ) {
 				clearInterval( intervalId );
 			}
 			attempts++;
