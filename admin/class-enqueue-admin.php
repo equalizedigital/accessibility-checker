30c765f95e18eecb8233d3cf4d877565ee0b4a65
pattonwebz <will@pattonwebz.com>
2025-06-30 14:24:57 +0100

    Update UTM parameter in fixesProUrl
    Changed the UTM parameter from 'utm-term' to 'utm-content' in the fixesProUrl for better tracking.
    
    [PRO-165]

--- admin/class-enqueue-admin.php
+++ admin/class-enqueue-admin.php
@@ -85,7 +85,7 @@ class Enqueue_Admin {
 					'nonce'       => wp_create_nonce( 'ajax-nonce' ),
 					'edacApiUrl'  => esc_url_raw( rest_url() . 'accessibility-checker/v1' ),
 					'restNonce'   => wp_create_nonce( 'wp_rest' ),
-					'fixesProUrl' => esc_url_raw( edac_generate_link_type( [ 'utm-term', '__fix__' ] ) ),
+					'fixesProUrl' => esc_url_raw( edac_generate_link_type( [ 'utm-content', '__fix__' ] ) ),
 				]
 			);
 
