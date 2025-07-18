39bed1174a37d6451e128a9ef8c92182cfb439c2
Steve Jones <steve@stevejonesdev.com>
2025-07-17 22:36:09 -0400

    fixed: correct grammar in 'how to fix' guidance for DuplicateFormLabelRule

--- includes/classes/Rules/Rule/DuplicateFormLabelRule.php
+++ includes/classes/Rules/Rule/DuplicateFormLabelRule.php
@@ -29,7 +29,7 @@ class DuplicateFormLabelRule implements RuleInterface {
 			'why_it_matters'        => esc_html__( 'Assistive technologies rely on a one-to-one relationship between form fields and labels to provide accurate guidance to users. When multiple labels are associated with the same field, it can confuse screen reader users and make form completion difficult as the screen reader may read the incorrect label for a field.', 'accessibility-checker' ),
 			'how_to_fix'            => sprintf(
 			// translators: %1$s is <code>&lt;label&gt;</code>, %2$s is <code>for=""</code>.
-				esc_html__( 'Ensure each form field has only one %1$s element associated with it. If multiple labels are present, remove extras or consolidate them into a single %1$s with a clear description. Make sure each %1$s element correctly references its form field using the %2$s attribute and that all elements on the page have unique IDs. This error happen when you have the same form embedded on a page twice and can be resolved by creating unique forms for different parts of a page rather than repeating the same form throughout.', 'accessibility-checker' ),
+				esc_html__( 'Ensure each form field has only one %1$s element associated with it. If multiple labels are present, remove extras or consolidate them into a single %1$s with a clear description. Make sure each %1$s element correctly references its form field using the %2$s attribute and that all elements on the page have unique IDs. This error happens when you have the same form embedded on a page twice and can be resolved by creating unique forms for different parts of a page rather than repeating the same form throughout.', 'accessibility-checker' ),
 				'<code>&lt;label&gt;</code>',
 				'<code>for=""</code>'
 			),
