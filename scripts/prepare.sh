27ff3c6ea82ce17453ce21c01c725066770d8305
pattonwebz <will@pattonwebz.com>
2025-07-21 21:05:45 +0100

    Remove wp-env related commands from prepare script
    Cleaned up the prepare.sh script by removing obsolete wp-env commands to streamline the setup process.

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,9 +1,7 @@
-# npm run start --update # wp-env removed
-# npm run stop # wp-env removed
+#!/bin/bash
 
 mkdir -p ./dist
 mkdir -p ./tests/e2e/.auth
-mkdir -p ./patches
 
 # note, when pulling from github you must include the commit hash
 # https://stackoverflow.com/questions/22608398/composer-update-not-pulling-latest-dev-master
