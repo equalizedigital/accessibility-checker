f0dd9f2925a83b0f2a6df4769a9c015df828a970
pattonwebz <will@pattonwebz.com>
2025-07-21 21:02:01 +0100

    Remove some no longer needed scripts and run commands

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -4,18 +4,12 @@
 mkdir -p ./dist
 mkdir -p ./tests/e2e/.auth
 mkdir -p ./patches
-# mkdir -p ./.wp-env # wp-env removed
 
 # note, when pulling from github you must include the commit hash
 # https://stackoverflow.com/questions/22608398/composer-update-not-pulling-latest-dev-master
 rm composer.lock
 composer install
 
-# cp -r -n ./vendor/equalizedigital/accessibility-checker-wp-env/.wp-env/ ./.wp-env # wp-env removed
-# cp -r -n ./vendor/equalizedigital/accessibility-checker-wp-env/.wp-env.json ./ # wp-env removed
-# cp -r ./vendor/equalizedigital/accessibility-checker-wp-env/patches/* ./patches/ # wp-env removed
-
-# npm run start # wp-env removed
-# ./.wp-env/scripts/init.sh # wp-env removed
+# npm run build
 npm run build
 
