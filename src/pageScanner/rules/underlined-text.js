341b2c80074e38a5bf82c6a2b6ef5a99b7f39aef
pattonwebz <will@pattonwebz.com>
2025-07-28 20:41:46 +0100

    Refactor underlined text rule to exclude links more effectively
    Updated the selector to prevent underlining for elements inside links, no need for this custom matcher anymore

--- src/pageScanner/rules/underlined-text.js
+++ src/pageScanner/rules/underlined-text.js
@@ -7,37 +7,11 @@
 
 export default {
 	id: 'underlined_text',
-	impact: 'moderate',
-	selector: '*:not(a):not(html):not(head):not(body)',
-	matches: ( element ) => {
-		// U tags we will always check.
-		if ( element.tagName.toLowerCase() === 'u' ) {
-			return true;
-		}
-
-		// Traverse up the dom 3 levels and check if the element is inside an anchor.
-		let parent = element.parentNode;
-		let isInsideAnchor = false;
-		for ( let i = 0; i < 3; i++ ) {
-			// Can't go further up the dom if the parent is the html element.
-			if ( parent.tagName.toLowerCase() === 'html' ) {
-				break;
-			}
-			if ( parent && parent.tagName.toLowerCase() === 'a' ) {
-				isInsideAnchor = true;
-				break;
-			}
-			parent = parent.parentNode;
-		}
-
-		// If in an anchor, don't check underline.
-		return ! isInsideAnchor;
-	},
-	tags: [ 'wcag324', 'wcag21aa', 'cat.text', 'custom' ],
+	excludeHidden: false,
+	selector: '*:not(a):not([role="link"]):not(a *):not([role="link"] *)',
 	metadata: {
 		description: 'Text elements should not be underlined unless they are links.',
 	},
 	all: [],
-	any: [],
-	none: [ 'element_is_u_tag', 'element_has_computed_underline' ],
+	none: [ 'element_with_underline', 'element_is_u_tag' ],
 };
