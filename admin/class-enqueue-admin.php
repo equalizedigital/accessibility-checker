3da0f7aeb2847bb59faf7afc61833c678bc9f29a
pattonwebz <will@pattonwebz.com>
2025-08-25 13:57:18 +0100

    Fix pro upsell tagging with utm-content

--- admin/class-enqueue-admin.php
+++ admin/class-enqueue-admin.php
@@ -86,7 +86,7 @@ class Enqueue_Admin {
 					'nonce'      => wp_create_nonce( 'ajax-nonce' ),
 					'edacApiUrl' => esc_url_raw( rest_url() . 'accessibility-checker/v1' ),
 					'restNonce'  => wp_create_nonce( 'wp_rest' ),
-					'proUrl'     => esc_url_raw( edac_generate_link_type( [ 'utm-content', '__name__' ] ) ),
+					'proUrl'     => esc_url_raw( edac_generate_link_type( [ 'utm-content' => '__name__' ] ) ),
 				]
 			);
 
