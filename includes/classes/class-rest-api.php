e798fb17f53a7bb5f113fb266f4810bdd103a320
pattonwebz <will@pattonwebz.com>
2025-12-20 00:31:33 +0000

    Allow a jwt token in request to validate for getting scan stats
    [PRO-286]

--- includes/classes/class-rest-api.php
+++ includes/classes/class-rest-api.php
@@ -11,6 +11,7 @@ use EDAC\Admin\Insert_Rule_Data;
 use EDAC\Admin\Scans_Stats;
 use EDAC\Admin\Settings;
 use EDAC\Admin\Purge_Post_Data;
+use EqualizeDigital\AccessibilityChecker\MyDot\Connector;
 
 /**
  * Class that initializes and handles the REST api
@@ -103,7 +104,10 @@ class REST_Api {
 					[
 						'methods'             => 'GET',
 						'callback'            => [ $this, 'get_scans_stats' ],
-						'permission_callback' => function () {
+						'permission_callback' => function ( $request ) {
+							if ( Connector::validate_jwt_token_in_request( $request ) ) {
+								return true;
+							}
 							return current_user_can( 'edit_posts' );
 						},
 					]
