5b85670ef58611ad362ec05311bf0f44dfacc5b9
pattonwebz <will@pattonwebz.com>
2026-01-14 22:28:25 +0000

    Add a minimal sidebar panel for use in the plugin for summary and details etc
    [PRO-500]

---
+++ src/sidebar/index.js
@@ -0,0 +1,32 @@
+/**
+ * Accessibility Checker Gutenberg Sidebar
+ */
+
+import { registerPlugin } from '@wordpress/plugins';
+import { PluginSidebar } from '@wordpress/editor';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Main sidebar component
+ */
+function AccessibilityCheckerSidebar() {
+	return (
+		<PluginSidebar
+			name="accessibility-checker-sidebar"
+			title={ __( 'Accessibility', 'accessibility-checker' ) }
+			icon="universal-access"
+		>
+			<div>
+				<p>{ __( 'Sidebar content goes here', 'accessibility-checker' ) }</p>
+			</div>
+		</PluginSidebar>
+	);
+}
+
+// Register the sidebar
+if ( window.edac_editor_app && window.edac_editor_app.gutenbergEnabled ) {
+	registerPlugin( 'accessibility-checker', {
+		render: AccessibilityCheckerSidebar,
+	} );
+}
+
