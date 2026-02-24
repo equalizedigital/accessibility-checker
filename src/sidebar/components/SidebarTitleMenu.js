25d1cf5195feb5e18425b780a64449d3145fcecd
pattonwebz <will@pattonwebz.com>
2026-02-24 14:50:43 +0000

    Handle closing of title menu and refocus back

--- src/sidebar/components/SidebarTitleMenu.js
+++ src/sidebar/components/SidebarTitleMenu.js
@@ -1,9 +1,28 @@
 import { __ } from '@wordpress/i18n';
 import { DropdownMenu, MenuGroup, MenuItem } from '@wordpress/components';
 import apiFetch from '@wordpress/api-fetch';
+import { useRef, useCallback } from '@wordpress/element';
 import { moreVertical, search, update, trash } from '@wordpress/icons';
 
 const SidebarTitleMenu = ( { postId, refetchData } ) => {
+	const menuRef = useRef( null );
+
+	/**
+	 * Close the dropdown and return focus to the toggle button.
+	 *
+	 * @param {Function} onClose - The dropdown's onClose callback.
+	 */
+	const closeAndRestoreFocus = useCallback( ( onClose ) => {
+		onClose();
+		// Allow the dropdown to fully close before restoring focus.
+		requestAnimationFrame( () => {
+			const toggleButton = menuRef.current?.querySelector( 'button' );
+			if ( toggleButton ) {
+				toggleButton.focus();
+			}
+		} );
+	}, [] );
+
 	const handleScan = () => {
 		document.dispatchEvent( new CustomEvent( 'edac-scan-requested', { detail: { success: true } } ) );
 	};
@@ -44,43 +63,45 @@ const SidebarTitleMenu = ( { postId, refetchData } ) => {
 	};
 
 	return (
-		<DropdownMenu
-			icon={ moreVertical }
-			label={ __( 'Sidebar actions', 'accessibility-checker' ) }
-			className="edac-sidebar__title-menu"
-		>
-			{ ( { onClose } ) => (
-				<MenuGroup>
-					<MenuItem
-						icon={ search }
-						onClick={ () => {
-							handleScan();
-							onClose();
-						} }
-					>
-						{ __( 'Scan', 'accessibility-checker' ) }
-					</MenuItem>
-					<MenuItem
-						icon={ update }
-						onClick={ () => {
-							handleRefresh();
-							onClose();
-						} }
-					>
-						{ __( 'Refresh', 'accessibility-checker' ) }
-					</MenuItem>
-					<MenuItem
-						icon={ trash }
-						onClick={ async () => {
-							await handleClearIssues();
-							onClose();
-						} }
-					>
-						{ __( 'Clear Issues', 'accessibility-checker' ) }
-					</MenuItem>
-				</MenuGroup>
-			) }
-		</DropdownMenu>
+		<div ref={ menuRef } className="edac-sidebar__title-menu-wrapper">
+			<DropdownMenu
+				icon={ moreVertical }
+				label={ __( 'Sidebar actions', 'accessibility-checker' ) }
+				className="edac-sidebar__title-menu"
+			>
+				{ ( { onClose } ) => (
+					<MenuGroup>
+						<MenuItem
+							icon={ search }
+							onClick={ () => {
+								handleScan();
+								closeAndRestoreFocus( onClose );
+							} }
+						>
+							{ __( 'Scan', 'accessibility-checker' ) }
+						</MenuItem>
+						<MenuItem
+							icon={ update }
+							onClick={ () => {
+								handleRefresh();
+								closeAndRestoreFocus( onClose );
+							} }
+						>
+							{ __( 'Refresh', 'accessibility-checker' ) }
+						</MenuItem>
+						<MenuItem
+							icon={ trash }
+							onClick={ async () => {
+								await handleClearIssues();
+								closeAndRestoreFocus( onClose );
+							} }
+						>
+							{ __( 'Clear Issues', 'accessibility-checker' ) }
+						</MenuItem>
+					</MenuGroup>
+				) }
+			</DropdownMenu>
+		</div>
 	);
 };
 
