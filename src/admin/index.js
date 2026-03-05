6ede5ecfd80f06c5a750428cafa522963ced7056
Steve Jones <steve@stevejonesdev.com>
2026-03-05 18:38:25 -0500

    Update: dashboard widget gradient color to use theme color variable

--- src/admin/index.js
+++ src/admin/index.js
@@ -724,7 +724,7 @@ const fillDashboardWidget = () => {
 						passedPercentage
 					);
 					passedPercentageEl.style.background =
-						'radial-gradient(closest-side, white 85%, transparent 80% 100%), conic-gradient(#006600 ' +
+						'radial-gradient(closest-side, white 85%, transparent 80% 100%), conic-gradient(var(--wp-admin-theme-color, #3273aa) ' +
 						passedPercentage +
 						'%, #e2e4e7 0)';
 				}
